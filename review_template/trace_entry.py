1132df2d0aca23b81451d105f03a5c1d3d202d07
Gerit Wagner <gerit.wagner@hec.ca>
2021-09-24 14:41:58 +0200

    update trace_entry

--- review_template/trace_entry.py
+++ review_template/trace_entry.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 import logging
+import pprint
 import time
 
 import bibtexparser
@@ -16,9 +17,10 @@ with open('shared_config.yaml') as shared_config_yaml:
     shared_config = yaml.load(shared_config_yaml, Loader=yaml.FullLoader)
 HASH_ID_FUNCTION = shared_config['params']['HASH_ID_FUNCTION']
 
-MAIN_REFERENCES = entry_hash_function.paths[HASH_ID_FUNCTION]['SCREEN']
+MAIN_REFERENCES = \
+    entry_hash_function.paths[HASH_ID_FUNCTION]['MAIN_REFERENCES']
 SCREEN = entry_hash_function.paths[HASH_ID_FUNCTION]['SCREEN']
-DATA = entry_hash_function.paths[HASH_ID_FUNCTION]['SCREEN']
+DATA = entry_hash_function.paths[HASH_ID_FUNCTION]['DATA']
 
 
 def trace_hash(bibfilename, hash_id_needed):
@@ -44,100 +46,68 @@ def trace_hash(bibfilename, hash_id_needed):
     return
 
 
-def main():
-    print('')
-    print('')
+def main(citation_key):
 
-    print('Trace entry by citation_key')
-
-    citation_key = input('provide citation_key')
-#    citation_key = 'Blijleven2019'
+    print('Trace entry by citation_key: ' + citation_key)
 
     repo = git.Repo()
 
     # TODO: trace_hash and list individual search results
 
-    path = MAIN_REFERENCES
+    revlist = repo.iter_commits()
 
-    revlist = (
-        (commit, (commit.tree / path).data_stream.read())
-        for commit in repo.iter_commits(paths=path)
-    )
+    pp = pprint.PrettyPrinter(indent=4)
+
     prev_entry = []
+    prev_screen = ''
+    prev_data = ''
+    for commit in reversed(list(revlist)):
 
-    for commit, filecontents in reversed(list(revlist)):
-        print('----------------------------------')
-        individual_bib_database = bibtexparser.loads(filecontents)
-        entry = [
-            entry for entry in individual_bib_database.entries
-            if entry['ID'] == citation_key
-        ]
-        if len(entry) != 0:
-            print(
-                str(commit),
-                ' - ',
-                commit.message.replace('\n', ''),
-                ' ',
-                commit.author.name,
-                ' ',
-                time.strftime(
-                    '%a, %d %b %Y %H:%M',
-                    time.gmtime(commit.committed_date),
-                ),
-            )
-            for diff in list(dictdiffer.diff(prev_entry, entry)):
-                print(diff)
-            prev_entry = entry
+        print('\n\nCommit: ' +
+              str(commit),
+              ' - ',
+              commit.message.partition('\n')[0],
+              ' ',
+              commit.author.name,
+              ' ',
+              time.strftime(
+                  '%a, %d %b %Y %H:%M',
+                  time.gmtime(commit.committed_date),
+              ),
+              )
 
-    path = SCREEN
+        if (MAIN_REFERENCES in commit.tree):
+            filecontents = (commit.tree / MAIN_REFERENCES).data_stream.read()
+            individual_bib_database = bibtexparser.loads(filecontents)
+            entry = [
+                entry for entry in individual_bib_database.entries
+                if entry['ID'] == citation_key
+            ]
 
-    revlist = (
-        (commit, (commit.tree / path).data_stream.read())
-        for commit in repo.iter_commits(paths=path)
-    )
+            if len(entry) == 0:
+                print('Entry ' + citation_key + ' not in commit.')
+            else:
+                diffs = list(dictdiffer.diff(prev_entry, entry))
+                if len(diffs) > 0:
+                    for diff in diffs:
+                        pp.pprint(diff)
+                prev_entry = entry
 
-    for commit, filecontents in reversed(list(revlist)):
-        print('----------------------------------')
-        print(
-            str(commit),
-            ' - ',
-            commit.message.replace('\n', ''),
-            ' ',
-            commit.author.name,
-            ' ',
-            time.strftime(
-                '%a, %d %b %Y %H:%M',
-                time.gmtime(commit.committed_date),
-            ),
-        )
-        for line in str(filecontents).split('\\n'):
-            if citation_key in line:
-                print(line)
+        if (SCREEN in commit.tree):
+            filecontents = (commit.tree / SCREEN).data_stream.read()
+            for line in str(filecontents).split('\\n'):
+                if citation_key in line:
+                    if line != prev_screen:
+                        print('Screen: ' + line)
+                        prev_screen = line
 
-    path = DATA
-
-    revlist = (
-        (commit, (commit.tree / path).data_stream.read())
-        for commit in repo.iter_commits(paths=path)
-    )
-
-    for commit, filecontents in reversed(list(revlist)):
-        print('----------------------------------')
-        print(
-            str(commit),
-            ' - ',
-            commit.message.replace('\n', ''),
-            ' ',
-            commit.author.name,
-            ' ',
-            time.strftime(
-                '%a, %d %b %Y %H:%M',
-                time.gmtime(commit.committed_date),
-            ),
-        )
-        for line in str(filecontents).split('\\n'):
-            if citation_key in line:
-                print(line)
+        if (DATA in commit.tree):
+            filecontents = (commit.tree / DATA).data_stream.read()
+            for line in str(filecontents).split('\\n'):
+                if citation_key in line:
+                    if line != prev_data:
+                        print('Data: ' + line)
+                        prev_data = line
 
 
 if __name__ == '__main__':
