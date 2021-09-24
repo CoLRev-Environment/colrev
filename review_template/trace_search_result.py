9d5ec97d60b89fecec65753c3b7ee08aeca879fa
Gerit Wagner <gerit.wagner@hec.ca>
2021-09-24 15:16:46 +0200

    update tracing

--- review_template/trace_search_result.py
+++ review_template/trace_search_result.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python
-import bibtexparser
+import pprint
+
 import yaml
 
 from review_template import entry_hash_function
+from review_template import importer
 from review_template import utils
 
 with open('shared_config.yaml') as shared_config_yaml:
@@ -10,42 +12,36 @@ with open('shared_config.yaml') as shared_config_yaml:
 HASH_ID_FUNCTION = shared_config['params']['HASH_ID_FUNCTION']
 
 
-def main():
-    print('')
-    print('')
+def main(search_file, id):
 
-    print('Trace an entry')
+    print('Trace search result: ' + search_file + ' - ' + id)
 
-    entry_string = input('Enter BibTeX entry (replacing \n newlines)')
+    # TODO: option: trace it through all the history
 
-#    entry_string = '@book{RN507, author = {Abdalla Mikhaeil, Christine ' + \
-#        'and Baskerville, Richard},  title = {An Identity Driven ' + \
-#        'Escalation of Commitment to Negative Spillovers},   series = ' + \
-#        '{ICIS 2017 Proceedings}, year = {2017},type = {Book}}'
-#    entry_string = entry_string.replace('\n', '')
-
-    entry_database = bibtexparser.loads(entry_string)
-
+    search_entries = importer.get_db_with_completion_edits(search_file)
+    # No need to check sufficiently_complete metadata:
+    # incomplete records should not be merged.
+    for entry in search_entries.entries:
+        hid = entry_hash_function.create_hash[HASH_ID_FUNCTION](entry)
+        entry.update(hash_id=hid)
+    target_entry = [x for x in search_entries.entries if id == x['ID']][0]
     bib_database = utils.load_references_bib(
         modification_check=False, initialize=False)
 
-    for entry in entry_database.entries:
-        hash_id = entry_hash_function.create_hash[HASH_ID_FUNCTION](entry)
+    pp = pprint.PrettyPrinter(indent=4)
 
-        print(hash_id)
-
-        found = False
-        for entry in bib_database.entries:
-            if entry['hash_id'] == hash_id:
-                print('citation_key: ' +
-                      entry['ID'] + ' for hash_id ' + entry['hash_id'])
-                print()
-                # if cleansed (hash_id in data/search/bib_details):
-                # add note: quality cleansed entry:
-                print(entry)
-                found = True
-        if not found:
-            print('Did not find entry')
+    found = False
+    for entry in bib_database.entries:
+        if entry.get('hash_id', 'NA') == target_entry['hash_id']:
+            print('citation_key: ' +
+                  entry['ID'] + ' for hash_id ' + entry['hash_id'])
+            print()
+            # if cleansed (hash_id in data/search/bib_details):
+            # add note: quality cleansed entry:
+            pp.pprint(entry)
+            found = True
+    if not found:
+        print('Did not find entry')
 
 
 if __name__ == '__main__':
