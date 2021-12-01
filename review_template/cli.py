b0758544a3044b1fa81726b5eac535a9c79a7163
Gerit Wagner <gerit.wagner@hec.ca>
2021-12-01 06:41:39 +0100

    move process to cli

--- review_template/cli.py
+++ review_template/cli.py
@@ -304,6 +304,26 @@ def status(ctx) -> None:
     return
 
 
+def reprocess_id(REVIEW_MANAGER, id: str, git_repo: git.Repo) -> None:
+    saved_args = locals()
+
+    MAIN_REFERENCES = REVIEW_MANAGER.paths["MAIN_REFERENCES"]
+    if "all" == id:
+        logging.info("Removing/reprocessing all records")
+        os.remove(MAIN_REFERENCES)
+        git_repo.index.remove([MAIN_REFERENCES], working_tree=True)
+
+    else:
+        bib_db = REVIEW_MANAGER.load_main_refs()
+        bib_db.entries = [x for x in bib_db.entries if id != x["ID"]]
+        REVIEW_MANAGER.save_bib_file(bib_db)
+        git_repo.index.add([MAIN_REFERENCES])
+
+    REVIEW_MANAGER.create_commit("Reprocess", saved_args)
+
+    return
+
+
 @main.command(help_priority=3)
 @click.pass_context
 @click.option("--reprocess", help='Record ID to reprocess ("all" to reprocess all).')
@@ -316,12 +336,31 @@ def status(ctx) -> None:
 )
 def process(ctx, reprocess_id, keep_ids) -> None:
     """Process records (automated steps)"""
-    from review_template import process
     from review_template.review_manager import ReviewManager
+    from review_template import dedupe
+    from review_template import load
+    from review_template import pdf_prepare
+    from review_template import pdf_get
+    from review_template import prepare
 
     try:
         REVIEW_MANAGER = ReviewManager()
-        process.main(REVIEW_MANAGER, reprocess_id, keep_ids)
+        # process.main(REVIEW_MANAGER, reprocess_id, keep_ids)
+        REVIEW_MANAGER.build_docker_images()
+        git_repo = REVIEW_MANAGER.get_repo()
+
+        if id is not None:
+            reprocess_id(reprocess_id, git_repo)
+
+        load.main(REVIEW_MANAGER, keep_ids)
+
+        prepare.main(REVIEW_MANAGER, keep_ids)
+
+        dedupe.main(REVIEW_MANAGER)
+
+        pdf_get.main(REVIEW_MANAGER)
+
+        pdf_prepare.main(REVIEW_MANAGER)
     except git.exc.InvalidGitRepositoryError:
         logging.error("No git repository found. Use review_template init")
         pass
