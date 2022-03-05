5c84f2fad2f0a17c04515ab65295afd4211b7224
Gerit Wagner <gerit.wagner@hec.ca>
2022-03-05 07:22:17 +0100

    update docs first page and includ logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ extensions = [
 
 # -- Project information -----------------------------------------------------
 
-project = "CoLRev"
+project = ""
 copyright = "2022, Gerit Wagner"
 author = "Gerit Wagner"
 
@@ -63,3 +63,6 @@ html_theme = "alabaster"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_logo = "logo_small.png"
+html_favicon = "favicon.png"
