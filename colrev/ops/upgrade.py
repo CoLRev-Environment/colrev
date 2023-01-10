d6715c2e43ea6576c6e575e93c160db61a39ed91
Gerit Wagner <gerit.wagner@hec.ca>
2023-01-10 15:19:12 +0100

    upgrade/status fix

--- colrev/ops/upgrade.py
+++ colrev/ops/upgrade.py
@@ -974,10 +974,13 @@ class Upgrade(colrev.operation.Operation):
                     o.startswith("md_local_index.bib")
                     for o in record_dict["colrev_origin"]
                 ):
+                    prev_status = record_dict["colrev_status"]
+
                     local_index_source.get_masterdata(
                         prep_operation=prep_operation,
                         record=colrev.record.Record(data=record_dict),
                     )
+                    record_dict["colrev_status"] = prev_status
 
         self.review_manager.dataset.save_records_dict(records=records)
 
