b1b0b5d5155928a00dc9106baf0d5854c8d25e5f
Gerit Wagner <geritwagner@users.noreply.github.com>
2023-04-05 11:39:24 +0200

    Update commit.py

--- colrev/ops/commit.py
+++ colrev/ops/commit.py
@@ -191,15 +191,12 @@ class Commit:
 
         return report
 
-    def create(self) -> bool:
+    def create(self, *, skip_status_yaml: bool = False) -> bool:
         """Create a commit (including the commit message and details)"""
 
         if self.review_manager.dataset.has_changes():
             self.review_manager.logger.debug("Prepare commit: checks and updates")
-            if (
-                "gh-pages"
-                != self.review_manager.dataset.get_repo().active_branch.branch_name
-            ):
+            if not skip_status_yaml:
                 self.review_manager.update_status_yaml()
                 self.review_manager.dataset.add_changes(
                     path=self.review_manager.STATUS_RELATIVE
