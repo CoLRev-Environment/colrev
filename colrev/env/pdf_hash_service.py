f287abbafff4266fd871a9dbea3d1443ab2b5fbd
Tarin Mahmood <34904+tmahmood@users.noreply.github.com>
2023-04-18 23:27:42 +0600

    Fixed codacy warnings
    Signed-off-by: Tarin Mahmood <34904+tmahmood@users.noreply.github.com>

--- colrev/env/pdf_hash_service.py
+++ colrev/env/pdf_hash_service.py
@@ -4,24 +4,14 @@ from __future__ import annotations
 
 import logging
 import os
-import subprocess
-import sys
-import tempfile
 from pathlib import Path
 
 import fitz
 import imagehash
 from PIL import Image
 
-import colrev.env.environment_manager
 import colrev.exceptions as colrev_exceptions
 import colrev.ui_cli.cli_colors as colors
-
-if False:  # pylint: disable=using-constant-test
-    from typing import TYPE_CHECKING
-
-    if TYPE_CHECKING:
-        import colrev.review_manager
 
 
 class PDFHashService:
@@ -47,24 +37,21 @@ class PDFHashService:
             raise colrev_exceptions.InvalidPDFException(path=pdf_path)
 
         doc: fitz.Document = fitz.open(pdf_path)
-        img = get_image(doc, page_nr)
-        res = imagehash.average_hash(img, hash_size=int(hash_size))
-        as_str = str(res).replace("\n", "")
-        if len(as_str) * "0" == as_str:
-            raise colrev_exceptions.PDFHashError(path=pdf_path)
-        return as_str
-
-
-def get_image(doc, page_nr):
-    file_name = f"page-{page_nr}.png"
-    with tempfile.TemporaryDirectory() as tp:
+        img = None
+        file_name = f"page-{page_nr}.png"
         page_no = 0
         for page in doc:
             pix = page.get_pixmap(dpi=200)
             pix.save(file_name)  # store image as a PNG
             page_no += 1
             if page_no == page_nr:
-                return Image.open(file_name)
+                img = Image.open(file_name)
+                break
+        res = imagehash.average_hash(img, hash_size=int(hash_size))
+        as_str = str(res).replace("\n", "")
+        if len(as_str) * "0" == as_str:
+            raise colrev_exceptions.PDFHashError(path=pdf_path)
+        return as_str
 
 
 if __name__ == "__main__":
