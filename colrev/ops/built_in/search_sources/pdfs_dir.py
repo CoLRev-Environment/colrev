7ac524217b4eb875954fb16b2526b073c143e73b
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2023-04-19 16:20:29 +0200

    refactor pdfs_dir part 2

--- colrev/ops/built_in/search_sources/pdfs_dir.py
+++ colrev/ops/built_in/search_sources/pdfs_dir.py
@@ -48,6 +48,7 @@ class PDFSearchSource(JsonSchemaMixin):
     )
 
     __doi_regex = re.compile(r"10\.\d{4,9}/[-._;/:A-Za-z0-9]*")
+    __batch_size = 20
 
     def __init__(
         self, *, source_operation: colrev.operation.CheckOperation, settings: dict
@@ -535,56 +536,55 @@ class PDFSearchSource(JsonSchemaMixin):
             return new_record
         return new_record
 
-    def run_search(
-        self, search_operation: colrev.ops.search.Search, rerun: bool
+    def __print_run_search_stats(
+        self, *, records: dict, nr_added: int, nr_changed: int
     ) -> None:
-        """Run a search of a PDF directory (based on GROBID)"""
-
-        # pylint: disable=too-many-locals
-        # pylint: disable=too-many-branches
-        # pylint: disable=too-many-statements
-
-        # Do not run in continuous-integration environment
-        if search_operation.review_manager.in_ci_environment():
-            return
-
-        # Removing records/origins for which PDFs were removed makes sense for curated repositories
-        # In regular repositories, it may be confusing (e.g., if PDFs are renamed)
-        # In these cases, we may simply print a warning instead of modifying/removing records?
-        if self.review_manager.settings.is_curated_masterdata_repo():
-            self.__remove_records_if_pdf_no_longer_exists(
-                search_operation=search_operation
+        if nr_added > 0:
+            self.review_manager.logger.info(
+                f"{colors.GREEN}Retrieved {nr_added} records{colors.END}"
+            )
+        else:
+            self.review_manager.logger.info(
+                f"{colors.GREEN}No additional records retrieved{colors.END}"
             )
 
-        records = search_operation.review_manager.dataset.load_records_dict()
-        grobid_service = search_operation.review_manager.get_grobid_service()
-        grobid_service.start()
+        if self.review_manager.force_mode:
+            if nr_changed > 0:
+                self.review_manager.logger.info(
+                    f"{colors.GREEN}Updated {nr_changed} records{colors.END}"
+                )
+            else:
+                if records:
+                    self.review_manager.logger.info(
+                        f"{colors.GREEN}Records (data/records.bib) up-to-date{colors.END}"
+                    )
 
-        pdfs_dir_feed = self.search_source.get_feed(
-            review_manager=search_operation.review_manager,
-            source_identifier=self.source_identifier,
-            update_only=(not rerun),
-        )
-
-        local_index = search_operation.review_manager.get_local_index()
-
+    def __get_pdf_batches(self) -> list:
         pdfs_to_index = [
-            x.relative_to(search_operation.review_manager.path)
+            x.relative_to(self.review_manager.path)
             for x in self.pdfs_path.glob("**/*.pdf")
         ]
 
-        linked_pdf_paths = [Path(r["file"]) for r in records.values() if "file" in r]
-
-        if search_operation.review_manager.force_mode:  # i.e., reindex all
-            search_operation.review_manager.logger.info("Reindex all")
-
-        batch_size = 20
         pdf_batches = [
-            pdfs_to_index[i * batch_size : (i + 1) * batch_size]
-            for i in range((len(pdfs_to_index) + batch_size - 1) // batch_size)
+            pdfs_to_index[i * self.__batch_size : (i + 1) * self.__batch_size]
+            for i in range(
+                (len(pdfs_to_index) + self.__batch_size - 1) // self.__batch_size
+            )
         ]
+        return pdf_batches
+
+    def __run_pdfs_dir_search(
+        self,
+        *,
+        search_operation: colrev.ops.search.Search,
+        records: dict,
+        pdfs_dir_feed: colrev.ops.search.GeneralOriginFeed,
+        local_index: colrev.env.local_index.LocalIndex,
+        linked_pdf_paths: list,
+        rerun: bool,
+    ) -> None:
         nr_added, nr_changed = 0, 0
-        for pdf_batch in pdf_batches:
+        for pdf_batch in self.__get_pdf_batches():
             for record in pdfs_dir_feed.feed_records.values():
                 record = self.__add_md_string(record_dict=record)
 
@@ -599,38 +599,26 @@ class PDFSearchSource(JsonSchemaMixin):
                 if new_record == {}:
                     continue
 
-                prev_record_dict_version = {}
-                if new_record["ID"] in pdfs_dir_feed.feed_records:
-                    prev_record_dict_version = pdfs_dir_feed.feed_records[
-                        new_record["ID"]
-                    ]
+                prev_record_dict_version = pdfs_dir_feed.feed_records.get(
+                    new_record["ID"], {}
+                )
 
                 added = pdfs_dir_feed.add_record(
                     record=colrev.record.Record(data=new_record),
                 )
                 if added:
                     nr_added += 1
+                    self.__add_doi_from_pdf_if_not_available(record_dict=new_record)
 
-                    if "doi" not in new_record:
-                        record = colrev.record.Record(data=new_record)
-                        record.set_text_from_pdf(
-                            project_path=search_operation.review_manager.path
-                        )
-                        res = re.findall(self.__doi_regex, record.data["text_from_pdf"])
-                        if res:
-                            record.data["doi"] = res[0].upper()
-                        del record.data["text_from_pdf"]
-
-                elif search_operation.review_manager.force_mode:
+                elif self.review_manager.force_mode:
                     # Note : only re-index/update
-                    changed = search_operation.update_existing_record(
+                    if search_operation.update_existing_record(
                         records=records,
                         record_dict=new_record,
                         prev_record_dict_version=prev_record_dict_version,
                         source=self.search_source,
                         update_time_variant_fields=rerun,
-                    )
-                    if changed:
+                    ):
                         nr_changed += 1
 
             for record in pdfs_dir_feed.feed_records.values():
@@ -638,25 +626,62 @@ class PDFSearchSource(JsonSchemaMixin):
 
             pdfs_dir_feed.save_feed_file()
 
-        if nr_added > 0:
-            search_operation.review_manager.logger.info(
-                f"{colors.GREEN}Retrieved {nr_added} records{colors.END}"
-            )
-        else:
-            search_operation.review_manager.logger.info(
-                f"{colors.GREEN}No additional records retrieved{colors.END}"
+        self.__print_run_search_stats(
+            records=records, nr_added=nr_added, nr_changed=nr_changed
+        )
+
+    def __add_doi_from_pdf_if_not_available(self, *, record_dict: dict) -> None:
+        if "doi" in record_dict:
+            return
+        record = colrev.record.Record(data=record_dict)
+        record.set_text_from_pdf(project_path=self.review_manager.path)
+        res = re.findall(self.__doi_regex, record.data["text_from_pdf"])
+        if res:
+            record.data["doi"] = res[0].upper()
+        del record.data["text_from_pdf"]
+
+    def run_search(
+        self, search_operation: colrev.ops.search.Search, rerun: bool
+    ) -> None:
+        """Run a search of a PDF directory (based on GROBID)"""
+
+        # Do not run in continuous-integration environment
+        if search_operation.review_manager.in_ci_environment():
+            return
+
+        if search_operation.review_manager.force_mode:  # i.e., reindex all
+            search_operation.review_manager.logger.info("Reindex all")
+
+        # Removing records/origins for which PDFs were removed makes sense for curated repositories
+        # In regular repositories, it may be confusing (e.g., if PDFs are renamed)
+        # In these cases, we may simply print a warning instead of modifying/removing records?
+        if self.review_manager.settings.is_curated_masterdata_repo():
+            self.__remove_records_if_pdf_no_longer_exists(
+                search_operation=search_operation
             )
 
-        if search_operation.review_manager.force_mode:
-            if nr_changed > 0:
-                search_operation.review_manager.logger.info(
-                    f"{colors.GREEN}Updated {nr_changed} records{colors.END}"
-                )
-            else:
-                if records:
-                    search_operation.review_manager.logger.info(
-                        f"{colors.GREEN}Records (data/records.bib) up-to-date{colors.END}"
-                    )
+        grobid_service = self.review_manager.get_grobid_service()
+        grobid_service.start()
+
+        local_index = self.review_manager.get_local_index()
+
+        records = self.review_manager.dataset.load_records_dict()
+        pdfs_dir_feed = self.search_source.get_feed(
+            review_manager=self.review_manager,
+            source_identifier=self.source_identifier,
+            update_only=(not rerun),
+        )
+
+        linked_pdf_paths = [Path(r["file"]) for r in records.values() if "file" in r]
+
+        self.__run_pdfs_dir_search(
+            search_operation=search_operation,
+            records=records,
+            pdfs_dir_feed=pdfs_dir_feed,
+            linked_pdf_paths=linked_pdf_paths,
+            local_index=local_index,
+            rerun=rerun,
+        )
 
     @classmethod
     def heuristic(cls, filename: Path, data: str) -> dict:
