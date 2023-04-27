05326fd0ab82b26e5a75ea62f48d41a936dfc224
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2023-04-27 07:15:21 +0200

    test: deactivate packages requiring docker in tests

--- colrev/ops/init.py
+++ colrev/ops/init.py
@@ -265,7 +265,7 @@ class Initializer:
                 + f": A {r_type_suffix} protocol",
             )
 
-        if self.light:
+        if self.light or colrev.review_manager.ReviewManager.in_ci_environment():
             settings.data.data_package_endpoints = [
                 x
                 for x in settings.data.data_package_endpoints
