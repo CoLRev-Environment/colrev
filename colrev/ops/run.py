670c56408684f8b3b466c22f25a62fe2bed88bd1
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2023-05-01 12:21:30 +0200

    use objects/methods

--- colrev/ops/run.py
+++ colrev/ops/run.py
@@ -5,22 +5,32 @@ from __future__ import annotations
 import bibtexparser
 
 
-def add_jif(*, record: dict) -> None:
-    """Add the journal impact factor"""
+class JIFLabeler:
+    """Object handling the JIF labeling"""
 
-    if record["journal"] == "MIS Quarterly":
-        record["journal_impact_factor"] = 8.3
-    if record["journal"] == "Information & Management":
-        record["journal_impact_factor"] = 10.3
+    def __init__(self) -> None:
+        pass
 
+    def add_jif(self, *, record: dict) -> None:
+        """Add the journal impact factor"""
+        if "journal" not in record:
+            return
+
+        if record["journal"] == "MIS Quarterly":
+            record["journal_impact_factor"] = 8.3
+        if record["journal"] == "Information & Management":
+            record["journal_impact_factor"] = 10.3
+
+    def run(self) -> None:
+        print("Start simple colrev run")
+
+        with open("data/records.bib", encoding="utf-8") as bibtex_file:
+            bib_database = bibtexparser.load(bibtex_file)
+
+        for record in bib_database.entries:
+            self.add_jif(record=record)
+            print(record)
 
 def main() -> None:
-    print("Start simple colrev run")
-
-    with open("records.bib", encoding="utf-8") as bibtex_file:
-        bib_database = bibtexparser.load(bibtex_file)
-
-    for record in bib_database.entries:
-        add_jif(record=record)
-        print(record)
-
+    jif_labeler = JIFLabeler()
+    jif_labeler.run()
