3205bf67ee3c1a87927d34117e447637aa7d09f3
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2023-05-04 11:49:31 +0200

    pdf_backward_search: conslidate with open-citations (for better data quality)

--- colrev/ops/built_in/search_sources/pdf_backward_search.md
+++ colrev/ops/built_in/search_sources/pdf_backward_search.md
@@ -6,12 +6,16 @@ Note: This document is currently under development. It will contain the followin
 - coverage (disciplines, types of work)
 - supported (details): search updates, get_masterdata, run_search, load_fixes, prep
 
+One strategy could be to start with a relatively high threshold for the number of intext citations and to iteratively decrease it, and update the search:
+colrev search -a colrev.pdf_backward_search:min_intext_citations=2
+
 based on [GROBID](https://github.com/kermitt2/grobid)
 
 ## Add the search source
 
 ```
-colrev search -a XXX
+colrev search -a colrev.pdf_backward_search:default
+colrev search -a colrev.pdf_backward_search:min_intext_citations=2
 ```
 
 ## Links
--- colrev/ops/built_in/search_sources/pdf_backward_search.py
+++ colrev/ops/built_in/search_sources/pdf_backward_search.py
@@ -2,16 +2,20 @@
 """SearchSource: backward search (based on PDFs and GROBID)"""
 from __future__ import annotations
 
+import json
 import typing
 from dataclasses import dataclass
 from pathlib import Path
 
+import requests
 import zope.interface
 from dacite import from_dict
 from dataclasses_jsonschema import JsonSchemaMixin
+from thefuzz import fuzz
 
 import colrev.env.package_manager
 import colrev.exceptions as colrev_exceptions
+import colrev.ops.built_in.search_sources.crossref
 import colrev.ops.search
 import colrev.record
 import colrev.ui_cli.cli_colors as colors
@@ -54,6 +58,12 @@ class BackwardSearchSource(JsonSchemaMixin):
             self.grobid_service.start()
 
         self.review_manager = source_operation.review_manager
+
+        self.crossref_connector = (
+            colrev.ops.built_in.search_sources.crossref.CrossrefSearchSource(
+                source_operation=source_operation
+            )
+        )
 
     @classmethod
     def get_default_source(cls) -> colrev.settings.SearchSource:
@@ -156,6 +166,101 @@ class BackwardSearchSource(JsonSchemaMixin):
                         f"{colors.GREEN}Records (data/records.bib) up-to-date{colors.END}"
                     )
 
+    def __get_reference_records(self, *, record_dict: dict) -> list:
+        references = []
+
+        url = f"https://opencitations.net/index/coci/api/v1/references/{record_dict['doi']}"
+        # headers = {"authorization": "YOUR-OPENCITATIONS-ACCESS-TOKEN"}
+        headers: typing.Dict[str, str] = {}
+
+        ret = requests.get(url, headers=headers, timeout=300)
+        try:
+            items = json.loads(ret.text)
+
+            for doi in [x["cited"] for x in items]:
+                try:
+                    retrieved_record = self.crossref_connector.query_doi(doi=doi)
+                    # if not crossref_query_return:
+                    #     raise colrev_exceptions.RecordNotFoundInPrepSourceException()
+                    retrieved_record.data["ID"] = retrieved_record.data["doi"]
+                    references.append(retrieved_record.data)
+                except (
+                    colrev_exceptions.RecordNotFoundInPrepSourceException,
+                    colrev_exceptions.RecordNotParsableException,
+                ):
+                    pass
+        except json.decoder.JSONDecodeError:
+            self.review_manager.logger.info(
+                f"Error retrieving citations from Opencitations for {record_dict['ID']}"
+            )
+
+        return references
+
+    def __get_similarity(
+        self, *, record: colrev.record.Record, retrieved_record_dict: dict
+    ) -> float:
+        title_similarity = fuzz.partial_ratio(
+            retrieved_record_dict.get("title", "NA").lower(),
+            record.data.get("title", "").lower(),
+        )
+        container_similarity = fuzz.partial_ratio(
+            colrev.record.PrepRecord(data=retrieved_record_dict)
+            .get_container_title()
+            .lower(),
+            record.get_container_title().lower(),
+        )
+        weights = [0.6, 0.4]
+        similarities = [title_similarity, container_similarity]
+
+        similarity = sum(similarities[g] * weights[g] for g in range(len(similarities)))
+        # logger.debug(f'record: {pp.pformat(record)}')
+        # logger.debug(f'similarities: {similarities}')
+        # logger.debug(f'similarity: {similarity}')
+        # pp.pprint(retrieved_record_dict)
+        return similarity
+
+    def __complement_with_open_citations_data(
+        self,
+        *,
+        pdf_backward_search_feed: colrev.ops.search.GeneralOriginFeed,
+        records: dict,
+    ) -> None:
+        self.review_manager.logger.info("Comparing records with open-citations data")
+        for parent_record_id in {
+            x["cited_by_ID"] for x in pdf_backward_search_feed.feed_records.values()
+        }:
+            parent_record = records[parent_record_id]
+
+            if "doi" not in parent_record:
+                continue
+
+            backward_references = self.__get_reference_records(
+                record_dict=parent_record
+            )
+            updated = 0
+            overall = 0
+            for feed_record_dict in pdf_backward_search_feed.feed_records.values():
+                if feed_record_dict["cited_by_ID"] != parent_record_id:
+                    continue
+                overall += 1
+                feed_record = colrev.record.Record(data=feed_record_dict)
+                max_similarity = 0.0
+                pos = -1
+                for i, backward_reference in enumerate(backward_references):
+                    similarity = self.__get_similarity(
+                        record=feed_record, retrieved_record_dict=backward_reference
+                    )
+                    if similarity > max_similarity:
+                        pos = i
+                        max_similarity = similarity
+                if max_similarity > 0.9:
+                    feed_record.data.update(**backward_references[pos - 1])
+                    # del backward_references[pos-1]
+                    updated += 1
+            self.review_manager.logger.info(
+                f" updated {updated}/{overall} records cited by {parent_record_id}"
+            )
+
     def run_search(
         self, search_operation: colrev.ops.search.Search, rerun: bool
     ) -> None:
@@ -213,7 +318,7 @@ class BackwardSearchSource(JsonSchemaMixin):
                     new_record["bwsearch_ref"] = (
                         record["ID"] + "_backward_search_" + new_record["ID"]
                     )
-                    new_record["cited_by_IDs"] = record["ID"]
+                    new_record["cited_by_ID"] = record["ID"]
                     new_record["cited_by_file"] = record["file"]
                     try:
                         pdf_backward_search_feed.set_id(record_dict=new_record)
@@ -246,6 +351,10 @@ class BackwardSearchSource(JsonSchemaMixin):
 
             except colrev_exceptions.TEIException:
                 search_operation.review_manager.logger.info("Eror accessing TEI")
+
+        self.__complement_with_open_citations_data(
+            pdf_backward_search_feed=pdf_backward_search_feed, records=records
+        )
 
         pdf_backward_search_feed.save_feed_file()
 
