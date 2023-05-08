fceb0f0b938bce93917a76b6f3dd8500d7c104d9
Tarin Mahmood <34904+tmahmood@users.noreply.github.com>
2023-05-08 15:19:53 +0600

    Fixed another codacy issue
    Signed-off-by: Tarin Mahmood <34904+tmahmood@users.noreply.github.com>

--- tests/3_built_in/exclude_complementary_materials_prep_test.py
+++ tests/3_built_in/exclude_complementary_materials_prep_test.py
@@ -7,18 +7,17 @@ import pytest
 import colrev.ops.built_in.prep.exclude_complementary_materials
 import colrev.ops.prep
 
+Cls = (
+    colrev.ops.built_in.prep.exclude_complementary_materials.ExcludeComplementaryMaterialsPrep
+)
+
 
 @pytest.fixture(scope="package", name="elp_ecm")
 def elp(
     prep_operation: colrev.ops.prep.Prep,
-) -> (
-    colrev.ops.built_in.prep.exclude_complementary_materials.ExcludeComplementaryMaterialsPrep
-):
+) -> Cls:
     """Fixture returning an ExcludeComplementaryMaterialsPrep instance"""
     settings = {"endpoint": "colrev.exclude_complementary_materials"}
-    Cls = (
-        colrev.ops.built_in.prep.exclude_complementary_materials.ExcludeComplementaryMaterialsPrep
-    )
     elp_instance = Cls(prep_operation=prep_operation, settings=settings)
     return elp_instance
 
@@ -51,7 +50,7 @@ PRESCREEN_INCLUDED = False
     ],
 )
 def test_prep_exclude_complementary_materials(
-    elp_ecm: colrev.ops.built_in.prep.exclude_complementary_materials.ExcludeComplementaryMaterialsPrep,
+    elp_ecm: Cls,
     input_value: dict,
     expected_outcome: bool,
 ) -> None:
