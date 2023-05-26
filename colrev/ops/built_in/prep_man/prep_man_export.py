fda881ffd326c11f6f7bafddf8e62a2400661cdc
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2023-05-26 09:48:06 +0200

    revise and document prep-man-export

--- colrev/ops/built_in/prep_man/prep_man_export.py
+++ colrev/ops/built_in/prep_man/prep_man_export.py
@@ -191,6 +191,15 @@ class ExportManPrep(JsonSchemaMixin):
     def __update_original_record_based_on_man_prepped(
         self, *, original_record: colrev.record.Record, man_prepped_record_dict: dict
     ) -> None:
+        dropped_keys = [
+            k
+            for k in original_record.data
+            if k in self.__FIELDS_TO_KEEP and k not in man_prepped_record_dict
+        ]
+        if man_prepped_record_dict["ENTRYTYPE"] != original_record.data["ENTRYTYPE"]:
+            original_record.data["ENTRYTYPE"] = man_prepped_record_dict["ENTRYTYPE"]
+            original_record.update_masterdata_provenance(qm=self.quality_model)
+
         for key, value in man_prepped_record_dict.items():
             if value != original_record.data.get(key, ""):
                 original_record.update_field(
@@ -200,11 +209,6 @@ class ExportManPrep(JsonSchemaMixin):
                     key=key, note="missing"
                 )
 
-        dropped_keys = [
-            k
-            for k in original_record.data
-            if k in self.__FIELDS_TO_KEEP and k not in man_prepped_record_dict
-        ]
         for dropped_key in dropped_keys:
             original_record.remove_field(
                 key=dropped_key, not_missing_note=True, source="man_prep"
@@ -249,6 +253,10 @@ class ExportManPrep(JsonSchemaMixin):
         imported_records: list,
     ) -> None:
         imported_records.append(original_record.data["ID"])
+        override = (
+            original_record.data["colrev_status"]
+            == colrev.record.RecordState.md_prepared
+        )
 
         self.__update_original_record_based_on_man_prepped(
             original_record=original_record,
@@ -256,20 +264,16 @@ class ExportManPrep(JsonSchemaMixin):
         )
 
         self.__drop_unnecessary_provenance_fiels(record=original_record)
-        override = (
-            original_record.data["colrev_status"]
-            == colrev.record.RecordState.md_prepared
-        )
+        if override:
+            original_record.set_status(
+                target_state=colrev.record.RecordState.md_prepared, force=True
+            )
         if (
             man_prepped_record_dict["colrev_status"]
             != colrev.record.RecordState.rev_prescreen_excluded
         ):
             original_record.update_masterdata_provenance(
                 qm=self.quality_model, set_prepared=True
-            )
-        if override:
-            original_record.set_status(
-                target_state=colrev.record.RecordState.md_prepared, force=True
             )
 
         self.__print_stats(original_record=original_record)
