442adf2712e20a4cc0a3eea2be6ce58f8ec6164a
ossendorfluca <luca@oausb.de>
2023-05-29 14:10:02 +0200

    started on creating the table

--- colrev/ops/dashboard.py
+++ colrev/ops/dashboard.py
@@ -12,21 +12,42 @@ import bibtexparser
 with open("./data/records.bib") as bibtex_file:     # changing file format to csv for pandas
     bib_database = bibtexparser.load(bibtex_file)
 df = pd.DataFrame(bib_database.entries)
-df.to_csv("./data/records.csv", index = False)
+df.to_csv("./data/records.csv", index = True)
+
 
 
 data = (                                            # the data we want to use later
     pd.read_csv("./data/records.csv")
     .query("colrev_status == 'rev_synthesized'")
 )
+data.rename(columns={'Unnamed: 0':'index'}, inplace=True)
+
+data.pop("ID")
+data.pop("abstract")
+data.pop("fulltext")
+data.pop("booktitle")
+data.pop("month")
+data.pop("publisher")
+data.pop("colrev_status")
+data.pop("colrev_masterdata_provenance")
+data.pop("colrev_origin")
+data.pop("colrev_data_provenance")
+data.pop("ENTRYTYPE")
+data.pop("dblp_key")
+data.pop("doi")
+data.pop("language")
 
 app = Dash(__name__)                                # initializing the dashboard app
 
 app.layout = html.Div(                              # defining th content
     children=[
-        html.H1(children="Colrev Dashboard"),
+        html.H1(children="currently synthesized records"),
+        html.Table([html.Tr([html.Td(col) for col in data.columns])] + 
+        [html.Tr([html.Td(data.iloc[i][col]) for col in data.columns]) for i in range(len(data))]
+        )
     ]
 )
+
 
 def main() -> None:
     print("Here comes the dashboard!!!")
