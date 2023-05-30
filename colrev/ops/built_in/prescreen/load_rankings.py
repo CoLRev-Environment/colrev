5acdd217262ce68d448cf5a56605767e75e72b82
Tester <lisa.smith@stud.uni-bamberg.de>
2023-05-30 18:53:39 +0200

    Rankings may be loaded as Directories with methods in colrev/ops/built_in/prescreen/load_rankings.py

---
+++ colrev/ops/built_in/prescreen/load_rankings.py
@@ -0,0 +1,29 @@
+import csv
+
+def __load_ais_ranking() -> dict():
+    ais_ranking = {}
+    with open('rankings_index.csv', mode='r', encoding = 'cp850') as f:
+        data = csv.reader(f,delimiter=',')
+        ais_ranking = {rows[0]:rows[1] for rows in data}
+    return ais_ranking
+
+def __load_vhb_ranking() -> dict():
+    vhb_ranking = {}
+    with open('rankings_index.csv', mode='r', encoding = 'cp850') as f:
+        data = csv.reader(f,delimiter=',')
+        vhb_ranking = {rows[2]:rows[3] for rows in data}
+    return vhb_ranking
+
+def __load_ft50_ranking() -> dict():
+    ft50_ranking = {}
+    with open('rankings_index.csv', mode='r', encoding = 'cp850') as f:
+        data = csv.reader(f,delimiter=',')
+        ft50_ranking = {rows[4]:rows[5] for rows in data}
+    return ft50_ranking
+
+def __load_predatory_journals_beall() -> dict():
+    predatory_journals_beall = {}
+    with open('rankings_index.csv', mode='r', encoding = 'cp850') as f:
+        data = csv.reader(f,delimiter=',')
+        predatory_journals_beall = {rows[6]:rows[7] for rows in data}
+    return predatory_journals_beall
