d17d60b6701f8570ef46dad76b77ff6c42970c64
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2023-06-09 14:10:56 +0200

    pubmed: drop redundant pii

--- colrev/ops/built_in/search_sources/pubmed.py
+++ colrev/ops/built_in/search_sources/pubmed.py
@@ -284,6 +284,11 @@ class PubMedSearchSource(JsonSchemaMixin):
         retrieved_record_dict = {
             k: v for k, v in retrieved_record_dict.items() if v != ""
         }
+        if (
+            retrieved_record_dict.get("pii", "pii").lower()
+            == retrieved_record_dict.get("doi", "doi").lower()
+        ):
+            del retrieved_record_dict["pii"]
 
         return retrieved_record_dict
 
