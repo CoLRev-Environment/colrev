38c3d8ec890448910ad54615189be74423d05f9d
Tarin Mahmood <34904+tmahmood@users.noreply.github.com>
2023-06-16 18:41:21 +0600

    Updated documentations, fixed some issues with conversion
    Signed-off-by: Tarin Mahmood <34904+tmahmood@users.noreply.github.com>

--- colrev/ops/built_in/load_conversion/ris_rispy_loader.md
+++ colrev/ops/built_in/load_conversion/ris_rispy_loader.md
@@ -4,21 +4,23 @@ Supported File type: `ris`
 
 ## Supported fields:
 
-"first_authors"
-"secondary_authors"
-"authors"
-Converted to "authors", also used to generate ID
+
+| RIS      | Mapped to                        |
+|:---------|:---------------------------------|
+| T2       | journal                          |
+| A1,A2,A3 | author, also used to generate ID |
+| Y1,PY    | year, also used to generate ID   |
+| SP,EP    | pages (SP--EP)                   |
+| IS       | Number                           |
+| N2       | abstract                         |
+| N1       | Note                             |
+| T1,TI    | Title                            |
+
+* Check Wiki for more details about available fields, right now only the fields
+mentioned here are available.
 
 Following fields are taken just as it is:
-"title",
-"primary_title",
-"journal",
-"volume",
-"year",
-"doi",
-"publisher",
-"starting_page"
-"final_page"
+"TI", "VL", "DO", "PB", "IS"
 
 ## Links
 * [rispy](https://pypi.org/project/rispy)
--- colrev/ops/built_in/load_conversion/ris_rispy_loader.py
+++ colrev/ops/built_in/load_conversion/ris_rispy_loader.py
@@ -2,6 +2,7 @@
 """Load conversion of bib files using rispy"""
 from __future__ import annotations
 
+import typing
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
@@ -32,21 +33,44 @@ class RisRispyLoader(JsonSchemaMixin):
 
     ci_supported: bool = True
 
+    fields_to_check = {
+        "title",
+        "primary_title",
+        "secondary_title",
+        "notes_abstract",
+        "note",
+        "volume",
+        "doi",
+        "publisher",
+        "number",
+    }
+    replace_with = {
+        "primary_title": "title",
+        "notes_abstract": "abstract",
+        "secondary_title": "journal",
+        "publication_year": "year",
+    }
+
     def __init__(
         self,
         *,
         load_operation: colrev.ops.load.Load,
         settings: dict,
     ) -> None:
+        self.entries = None
         self.settings = self.settings_class.load_settings(data=settings)
 
     def load(
         self, load_operation: colrev.ops.load.Load, source: colrev.settings.SearchSource
     ) -> dict:
         """Load records from the source"""
-        records = {}
+        records: dict = {}
         if source.filename.is_file():
-            records = self.__parse_ris_file_and_convert_to_bib(source)
+            with open(source.filename, encoding="utf-8") as ris_file:
+                self.entries = rispy.load(file=ris_file)
+            if self.entries:
+                records = self.__parse_ris_file_and_convert_to_bib()
+
         endpoint_dict = load_operation.package_manager.load_packages(
             package_type=colrev.env.package_manager.PackageEndpointType.search_source,
             selected_packages=[source.get_dict()],
@@ -54,60 +78,56 @@ class RisRispyLoader(JsonSchemaMixin):
             ignore_not_available=False,
         )
         endpoint = endpoint_dict[source.endpoint]
-
         records = endpoint.load_fixes(  # type: ignore
             load_operation, source=source, records=records
         )
         return records
 
-    def __parse_ris_file_and_convert_to_bib(
-        self, source: colrev.settings.SearchSource
-    ) -> dict:
+    def __get_entry_id(self, entry: dict) -> typing.Tuple[str, str, int]:
+        try:
+            authors = entry["first_authors"] + entry["secondary_authors"]
+        except KeyError:
+            authors = entry["authors"]
+        first_three_authors = authors
+        if len(authors) >= 3:
+            first_three_authors = authors[:3]
+        try:
+            year = entry["year"].strip("/")
+        except KeyError:
+            year = entry["publication_year"].strip("/")
+        three_author = "".join(
+            [author.rsplit(",")[0].strip() for author in first_three_authors]
+        )
+        _id = f"{three_author.lower()}{year}"
+        return _id, authors, year
+
+    def __add_entry(self, _entry: dict, key: str, record: dict) -> None:
+        if key in _entry:
+            val = _entry[key]
+            target_key = key if key not in self.replace_with else self.replace_with[key]
+            record[target_key] = val
+
+    def __parse_ris_file_and_convert_to_bib(self) -> dict | None:
         """Parses given ris file with rispy and convert it to bib record"""
-
-        def add_entry(_entry: dict, key: str, _record: dict) -> None:
-            if key in _entry:
-                _record[key] = _entry[key]
-
-        with open(source.filename, encoding="utf-8") as ris_file:
-            entries = rispy.load(file=ris_file)
-
+        if not self.entries:
+            return None
         records = {}
-        for entry in entries:
-            print(entry)
+        for entry in self.entries:
+            entry = dict(sorted(entry.items()))
             # colrev uses first 3 auther + year format
-            try:
-                authors = entry["authors"]
-            except KeyError:
-                authors = entry["first_authors"] + entry["secondary_authors"]
-            first_three_authors = authors
-            if len(authors) >= 3:
-                first_three_authors = authors[:3]
-            try:
-                year = entry["year"]
-            except KeyError:
-                year = entry["publication_year"].strip("/")
-            three_author = "".join(
-                [author.rsplit(",")[0].strip() for author in first_three_authors]
-            )
-            _id = f"{three_author.lower()}{year}"
+            _id, authors, year = self.__get_entry_id(entry)
             record = {
                 "ID": _id,
                 "author": " and ".join(authors),
                 "ENTRYTYPE": "article",
+                "year": year,
             }
-            fields_to_check = [
-                "title",
-                "primary_title",
-                "journal",
-                "volume",
-                "year",
-                "doi",
-                "publisher",
-            ]
-            for field in fields_to_check:
-                add_entry(entry, field, record)
+            for field in self.fields_to_check:
+                self.__add_entry(entry, field, record)
             if "starting_page" in entry and "final_page" in entry:
                 record["pages"] = f"{entry['starting_page']}--{entry['final_page']}"
+            elif "starting_page" in entry:
+                record["pages"] = f"{entry['starting_page']}--"
+
             records[_id] = record
         return records
