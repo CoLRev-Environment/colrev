94ccdc9082ce3ba34dc9bfdf68fa1f5a775f6451
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2023-09-25 14:39:06 +0200

    simplify abi-inform-proquest

--- colrev/ops/built_in/search_sources/abi_inform_proquest.py
+++ colrev/ops/built_in/search_sources/abi_inform_proquest.py
@@ -43,6 +43,7 @@ class ABIInformProQuestSearchSource(JsonSchemaMixin):
     ) -> None:
         self.search_source = from_dict(data_class=self.settings_class, data=settings)
         self.quality_model = source_operation.review_manager.get_qm()
+        self.review_manager = source_operation.review_manager
 
     @classmethod
     def heuristic(cls, filename: Path, data: str) -> dict:
@@ -76,6 +77,38 @@ class ABIInformProQuestSearchSource(JsonSchemaMixin):
         """Not implemented"""
         return record
 
+    def __remove_duplicates(self, *, records: dict) -> None:
+        to_delete = []
+        for record in records.values():
+            if re.search(r"-\d{1,2}$", record["ID"]):
+                original_record_id = re.sub(r"-\d{1,2}$", "", record["ID"])
+                if original_record_id not in records:
+                    continue
+                original_record = records[original_record_id]
+
+                # Note: between duplicate records,
+                # there are variations in spelling and completeness
+                if (
+                    colrev.record.Record.get_record_similarity(
+                        record_a=colrev.record.Record(data=record),
+                        record_b=colrev.record.Record(data=original_record),
+                    )
+                    < 0.9
+                ):
+                    continue
+
+                if original_record_id not in records:
+                    continue
+                to_delete.append(record["ID"])
+        if to_delete:
+            for rid in to_delete:
+                self.review_manager.logger.info(f" remove duplicate {rid}")
+                del records[rid]
+
+            self.review_manager.dataset.save_records_dict_to_file(
+                records=records, save_path=self.search_source.filename
+            )
+
     def load(self, load_operation: colrev.ops.load.Load) -> dict:
         """Load the records from the SearchSource file"""
 
@@ -83,38 +116,7 @@ class ABIInformProQuestSearchSource(JsonSchemaMixin):
             records = colrev.ops.load_utils_bib.load_bib_file(
                 load_operation=load_operation, source=self.search_source
             )
-            to_delete = []
-            for record in records.values():
-                if re.search(r"-\d{1,2}$", record["ID"]):
-                    original_record_id = re.sub(r"-\d{1,2}$", "", record["ID"])
-                    if original_record_id not in records:
-                        continue
-                    original_record = records[original_record_id]
-
-                    # Note: between duplicate records,
-                    # there are variations in spelling and completeness
-                    if (
-                        colrev.record.Record.get_record_similarity(
-                            record_a=colrev.record.Record(data=record),
-                            record_b=colrev.record.Record(data=original_record),
-                        )
-                        < 0.9
-                    ):
-                        continue
-
-                    if original_record_id not in records:
-                        continue
-                    to_delete.append(record["ID"])
-            if to_delete:
-                for rid in to_delete:
-                    load_operation.review_manager.logger.info(
-                        f" remove duplicate {rid}"
-                    )
-                    del records[rid]
-
-                load_operation.review_manager.dataset.save_records_dict_to_file(
-                    records=records, save_path=self.search_source.filename
-                )
+            self.__remove_duplicates(records=records)
             return records
 
         raise NotImplementedError
