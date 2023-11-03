9ecf5ea2fd31b1c7a32cfe3c58eb1cb500ded910
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2023-11-03 17:13:54 +0100

    format download_from_website

--- colrev/ops/built_in/pdf_get/download_from_website.py
+++ colrev/ops/built_in/pdf_get/download_from_website.py
@@ -28,7 +28,8 @@ class WebsiteDownload(JsonSchemaMixin):
     ci_supported: bool = False
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        + "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate, br",
         "Connection": "keep-alive",
