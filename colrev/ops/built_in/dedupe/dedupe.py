7f916a6661c05824924e28501635f550901cb0f9
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2023-11-09 07:38:31 +0100

    checks and tests

--- colrev/ops/built_in/dedupe/dedupe.py
+++ colrev/ops/built_in/dedupe/dedupe.py
@@ -14,6 +14,7 @@ from rapidfuzz import fuzz
 import colrev.env.package_manager
 import colrev.ops.built_in.dedupe.utils
 import colrev.record
+from colrev.constants import Colors
 from colrev.constants import Fields
 
 if TYPE_CHECKING:
@@ -115,14 +116,13 @@ class Dedupe(JsonSchemaMixin):
     def __calc_similarities(self, pairs_df: pd.DataFrame) -> pd.DataFrame:
         # Add similarities if both fields exist
         similarity_fields = [
+            Fields.AUTHOR,
             Fields.TITLE,
             Fields.YEAR,
             Fields.VOLUME,
-            Fields.NUMBER,
             Fields.PAGES,
             Fields.ABSTRACT,
             Fields.ISBN,
-            Fields.AUTHOR,
             Fields.DOI,
         ]
         for similarity_field in similarity_fields:
@@ -140,6 +140,7 @@ class Dedupe(JsonSchemaMixin):
 
         similarity_fields = [
             Fields.CONTAINER_TITLE,
+            Fields.NUMBER,  # some journals have numbers like 3/4 (which can be abbreviated)
         ]
         for similarity_field in similarity_fields:
             # TODO : how to deal with NA?
@@ -153,6 +154,14 @@ class Dedupe(JsonSchemaMixin):
                 else 0,
                 axis=1,
             )
+
+        pairs_df["title_partial_ratio"] = pairs_df.apply(
+            lambda row: fuzz.partial_ratio(str(row["title_1"]), str(row["title_2"]))
+            / 100
+            if row["title_1"] is not None and row["title_2"] is not None
+            else 0,
+            axis=1,
+        )
 
     def __create_pairs_for_block_fields(
         self, records_df: pd.DataFrame, block_fields: list
@@ -187,7 +196,6 @@ class Dedupe(JsonSchemaMixin):
 
         # TODO : think: how do we measure similarity for missing values?
 
-        # TODO : conditions that are redundant?
         # Queries are better for debugging (understanding which conditions do/do not apply)
         # https://jakevdp.github.io/PythonDataScienceHandbook/03.12-performance-eval-and-query.html
         # pylint: disable=line-too-long
@@ -195,7 +203,6 @@ class Dedupe(JsonSchemaMixin):
             f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.VOLUME} > 0.8 & {Fields.PAGES} > 0.8 & {Fields.ABSTRACT} > 0.9 & {Fields.ISBN} > 0.99)",
             f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.CONTAINER_TITLE} > 0.6 & {Fields.VOLUME} > 0.8 & {Fields.PAGES} > 0.8 & {Fields.ABSTRACT} > 0.9)",
             f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.CONTAINER_TITLE} > 0.6 & {Fields.NUMBER} > 0.8 & {Fields.PAGES} > 0.8 & {Fields.ABSTRACT} > 0.9)",
-            f"({Fields.AUTHOR} > 0.9 & {Fields.VOLUME} > 0.8 & {Fields.TITLE} > 0.9 & {Fields.CONTAINER_TITLE} > 0.6 & {Fields.NUMBER} > 0.8 & {Fields.ABSTRACT} > 0.9)",
             f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.VOLUME} > 0.8 & {Fields.NUMBER} > 0.8 & {Fields.ABSTRACT} > 0.9)",
             f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.VOLUME} > 0.8 & {Fields.PAGES} > 0.8 & {Fields.ABSTRACT} > 0.9)",
             f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.NUMBER} > 0.8 & {Fields.PAGES} > 0.8 & {Fields.ABSTRACT} > 0.9)",
@@ -205,19 +212,21 @@ class Dedupe(JsonSchemaMixin):
             f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.CONTAINER_TITLE} > 0.75 & {Fields.VOLUME} > 0.8 & {Fields.PAGES} > 0.8 & {Fields.ABSTRACT} > 0.8)",
             f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.CONTAINER_TITLE} > 0.75 & {Fields.NUMBER} > 0.8 & {Fields.PAGES} > 0.8 & {Fields.ABSTRACT} > 0.8)",
             f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.CONTAINER_TITLE} > 0.75 & {Fields.VOLUME} > 0.8 & {Fields.NUMBER} > 0.8 & {Fields.ABSTRACT} > 0.8)",
+            f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.CONTAINER_TITLE} > 0.6 & {Fields.VOLUME} > 0.8 & {Fields.NUMBER} > 0.8 & {Fields.ABSTRACT} > 0.9)",
             f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.CONTAINER_TITLE} > 0.7 & {Fields.ABSTRACT} > 0.9)",
-            f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.ABSTRACT} > 0.9 & {Fields.ISBN} > 0.99)",
             f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.CONTAINER_TITLE} > 0.6 & {Fields.NUMBER} > 0.9 & {Fields.PAGES} > 0.9)",
-            f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.VOLUME} > 0.9 & {Fields.NUMBER} > 0.9 & {Fields.ISBN} > 0.99)",
             f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.CONTAINER_TITLE} > 0.6 & {Fields.VOLUME} > 0.9 & {Fields.PAGES} > 0.9)",
-            f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.NUMBER} > 0.9 & {Fields.PAGES} > 0.9 & {Fields.ISBN} > 0.99)",
             f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.95 & {Fields.CONTAINER_TITLE} > 0.9 & {Fields.VOLUME} > 0.8 & {Fields.PAGES} > 0.8)",
             f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.95 & {Fields.CONTAINER_TITLE} > 0.9 & {Fields.VOLUME} > 0.8 & {Fields.NUMBER} > 0.8)",
             f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.95 & {Fields.CONTAINER_TITLE} > 0.9 & {Fields.NUMBER} > 0.8 & {Fields.PAGES} > 0.8)",
-            f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.95 & {Fields.VOLUME} > 0.8 & {Fields.PAGES} > 0.8 & {Fields.ISBN} > 0.99)",
-            f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.95 & {Fields.VOLUME} > 0.8 & {Fields.PAGES} > 0.8 & {Fields.ISBN} > 0.99)",
-            f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.95 & {Fields.VOLUME} > 0.8 & {Fields.PAGES} > 0.8 & {Fields.ISBN} > 0.99)",
             f"({Fields.AUTHOR} > 0.8 & {Fields.TITLE} > 0.99 & {Fields.CONTAINER_TITLE} > 0.99 & {Fields.DOI} > 0.99)",
+            f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.VOLUME} > 0.9 & {Fields.NUMBER} > 0.9 & {Fields.ISBN} > 0.99)",
+            f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.95 & {Fields.VOLUME} > 0.8 & {Fields.PAGES} > 0.8 & {Fields.ISBN} > 0.99)",
+            f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.95 & {Fields.VOLUME} > 0.8 & {Fields.PAGES} > 0.8 & {Fields.ISBN} > 0.99)",
+            f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.95 & {Fields.VOLUME} > 0.8 & {Fields.PAGES} > 0.8 & {Fields.ISBN} > 0.99)",
+            f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.ABSTRACT} > 0.9 & {Fields.ISBN} > 0.99)",
+            f"({Fields.AUTHOR} > 0.9 & {Fields.TITLE} > 0.9 & {Fields.NUMBER} > 0.9 & {Fields.PAGES} > 0.9 & {Fields.ISBN} > 0.99)",
+            f"({Fields.AUTHOR} > 0.95 & title_partial_ratio > 0.8 & {Fields.CONTAINER_TITLE} > 0.99 & {Fields.DOI} > 0.99)",
             f'({Fields.ENTRYTYPE}_1 == "inproceedings" & {Fields.ENTRYTYPE}_2 == "inproceedings" & {Fields.CONTAINER_TITLE} > 0.6 & {Fields.TITLE} > 0.9 & {Fields.AUTHOR} > 0.8 & {Fields.YEAR} > 0.9)',
         ]
 
@@ -227,33 +236,15 @@ class Dedupe(JsonSchemaMixin):
                 print("True merge conditions:")
                 for query in queries:
                     if pairs.query(query).shape[0] > 0:
-                        print(query)
+                        print(f"{Colors.GREEN}{query}{Colors.END}")
+                    else:
+                        print(f"{Colors.RED}{query}{Colors.END}")
 
         true_pairs = pairs.query("|".join(queries))
 
         print("TODO : continue here!")
 
         # TODO : the prevented-same-source merges should go into the manual list
-        # Find papers with low matching dois - often indicates FALSE positive matches
-        # true_pairs_mismatch_doi = true_pairs[
-        #     (~pd.isna(true_pairs[Fields.DOI])) & (true_pairs[Fields.DOI] > 0) & (true_pairs[Fields.DOI] <= 0.99) &
-        #     ~((true_pairs[Fields.TITLE] > 0.9) & (true_pairs[Fields.ABSTRACT] > 0.9) & ((true_pairs[Fields.CONTAINER_TITLE] > 0.9) | (true_pairs[Fields.ISBN] > 0.9)))
-        # ]
-
-        # Remove papers with low matching dois from filtered matched
-        # true_pairs = true_pairs[
-        #     # (pd.isna(true_pairs[Fields.DOI]))
-        #     # | (true_pairs[Fields.DOI] > 0.99)
-        #     # | (true_pairs[Fields.DOI] == 0)
-        #     # | ~(
-        #     #     (true_pairs[Fields.TITLE] > 0.9)
-        #     #     & (true_pairs[Fields.ABSTRACT] > 0.9)
-        #     #     & (
-        #     #         (true_pairs[Fields.CONTAINER_TITLE] > 0.9)
-        #     #         | (true_pairs[Fields.ISBN] > 0.9)
-        #     #     )
-        #     # )
-        # ]
 
         # exclude conditions
         queries = [
@@ -266,11 +257,15 @@ class Dedupe(JsonSchemaMixin):
                 print("Exclude conditions:")
                 for query in queries:
                     if pairs.query(query).shape[0] == 0:
+                        print(f"{Colors.RED}{query}{Colors.END}")
+                    else:
                         print(query)
 
         true_pairs = true_pairs.query("~(" + " | ".join(queries) + ")")
 
         true_pairs = true_pairs.drop_duplicates()
+
+        maybe_pairs = pd.DataFrame()
 
         # # Make year numeric, then find matches where year differs
         # true_pairs['year1'] = pd.to_numeric(true_pairs['year1'], errors='coerce')
@@ -313,8 +308,7 @@ class Dedupe(JsonSchemaMixin):
         # maybe_pairs = pd.concat([maybe_pairs, important_mismatch])
         # maybe_pairs = maybe_pairs.drop_duplicates()
 
-        # # true_pairs = true_pairs[['author1', 'author2', 'title1', 'title2', 'year1', 'year2', 'journal1', 'journal2', 'doi1', 'doi2', 'record_ID1', 'record_ID2']]
-        maybe_pairs = pd.DataFrame()
+
         return {"true_pairs": true_pairs, "maybe_pairs": maybe_pairs}
 
     def run_dedupe(self) -> None:
@@ -327,18 +321,6 @@ class Dedupe(JsonSchemaMixin):
 
         records = self.dedupe_operation.prep_records(records_df=records_df)
         records_df = pd.DataFrame.from_dict(records, orient="index")
-
-        if self.dedupe_operation.debug:
-            # TODO : the dedupe is benchmarked on the historical dataset?! -> move debug to the jupyter notebook?
-            self.review_manager.verbose_mode = True
-            origin1, origin2 = input("Provide ids (separated by ;)").split(";")
-            records_df = records_df[
-                records_df["colrev_origin"].apply(
-                    lambda x: origin1 in x or origin2 in x
-                )
-            ]
-            self.review_manager.p_printer.pprint(records_df.iloc[0].to_dict())
-            self.review_manager.p_printer.pprint(records_df.iloc[1].to_dict())
 
         deduplication_pairs = self.block_pairs_for_deduplication(records_df)
         deduplication_pairs.to_csv("dedupe_pairs.csv")
