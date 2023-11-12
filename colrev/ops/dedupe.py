7d8f95cd63c567f5543a3f808af8b2ef25dd074c
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2023-11-12 10:12:25 +0100

    update imports/dedupe

--- colrev/ops/dedupe.py
+++ colrev/ops/dedupe.py
@@ -5,12 +5,10 @@ from __future__ import annotations
 import re
 import string
 import typing
-from collections import Counter
-from collections import defaultdict
+from collections import Counter, defaultdict
 from itertools import combinations
 from pathlib import Path
-from typing import List
-from typing import Optional
+from typing import List, Optional
 from typing import TYPE_CHECKING
 
 import pandas as pd
