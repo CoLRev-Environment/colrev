48d6f7c15f583782c8c75d61080c30847bf2347b
Robert Theis <robert.theis@stud.uni-bamberg.de>
2023-11-27 22:50:48 +0100

    add functions and implementations to UI class

--- colrev/ops/built_in/search_sources/semanticscholar_ui.py
+++ colrev/ops/built_in/search_sources/semanticscholar_ui.py
@@ -3,11 +3,74 @@ import inquirer
 
 class Semanticscholar_ui():
 
+    '''Implements the User Interface for the SemanticScholar API Search within colrev'''
+
+    searchSubject: str
+    searchParams: dict
+
+    #def __init__(self):
+    '''__init__ necessary here??'''
+     #   searchSubject = None
+      #  searchParams = None
+
+
+    def mainUI(self) -> None:
+
+        '''Display the main Menu and choose the search type'''
+
+        print("Welcome to SemanticScholar! \n\n")
+        mainMsg = "Please choose one of the options below: "
+        mainOptions = ["Search for paper", "Search for author", "Keyword search"]
+
+        fwdValue = self.chooseOption(msg=mainMsg, options=mainOptions)
+
+        if fwdValue == "Search for paper":
+            self.searchSubject = "paper"
+            self.paperUI()
+        
+        elif fwdValue == "Search for author":
+            self.searchSubject = "author"
+            self.authorUI
+
+        elif fwdValue == "Keyword search":
+            self.searchSubject = "keyword"
+            self.keywordUI
+
+
+    def paperUI(self) -> None:
+        
+        '''Ask user to enter search parameters for distinctive paper search'''
+
+        pMsg = "How would you like to search for the paper?"
+        pOptions = ["S2PaperId", "CorpusId", "DOI", "ArXivId", "MAG", "ACL", "PMID", "PMCID", "URL", "Search by title"]
+
+        param = self.chooseOption(msg=pMsg, options=pOptions)
+
+        if param in pOptions and not (param == "Search by title"):
+            paramValue = self.enterText(msg="Please enter the chosen ID in the right format: ")
+            self.searchParams[param] = paramValue
+
+        elif param == "Search by title":
+            paramValue = self.enterText(msg="Please enter the title of the paper: ")
+            self.searchParams[param] = paramValue
+        
+
+    def authorUI(self) -> None:
+        '''Not implemented'''
+        print("Success!")
+
+    def keywordUI(self) -> None:
+        '''Not implemented'''
+        print("Success!")
+
+
+
+
     def chooseOption(self,
                      *,
                      msg: str,
                      options
-                     ) -> dict:
+                     ) -> str:
         question = [inquirer.List(name="Choice",
                                   message=msg,
                                   choices=["%s" % i for i in options],
@@ -15,25 +78,23 @@ class Semanticscholar_ui():
                                   ),
                     ]
         choice = inquirer.prompt(questions=question)
-        print(list(choice.values()))
-        return choice
 
-def main():
-    myList = [
-        'author',
-        'whatever',
-        'something'
-    ]
+        return choice.values
+    
+    def enterText(self,
+                  *,
+                  msg: str,
+                  options
+                  ) -> str:
+        question = [inquirer.Text(name="Entry", message=msg,)]
+        choice = inquirer.prompt(questions=question)
 
-    test = Semanticscholar_ui()
-    choice = test.chooseOption(msg="Choose something:", options=myList)
-    print(list(choice.values()))
+        return choice.values
+    
 
-    if 'author' in list(choice.values()) :
-        print("Author has been selected")
-
-main()
-
+#if __name__ == "mainUI":
+ #   test = Semanticscholar_ui()
+  #  test.mainUI()
 
 
 
