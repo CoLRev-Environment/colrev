d6fe9723bd57777c68b1e242d508d639c4f81b83
Robert Theis <robert.theis@stud.uni-bamberg.de>
2023-11-28 13:14:13 +0100

    add functions and further side menues to ui

--- colrev/ops/built_in/search_sources/semanticscholar_ui.py
+++ colrev/ops/built_in/search_sources/semanticscholar_ui.py
@@ -41,29 +41,98 @@ class Semanticscholar_ui():
         
         '''Ask user to enter search parameters for distinctive paper search'''
 
-        pMsg = "How would you like to search for the paper?"
-        pOptions = ["S2PaperId", "CorpusId", "DOI", "ArXivId", "MAG", "ACL", "PMID", "PMCID", "URL", "Search by title"]
+        paperList = list[str]
+        morePapers = True
 
-        param = self.chooseOption(msg=pMsg, options=pOptions)
+        while morePapers:
 
-        if param in pOptions and not (param == "Search by title"):
-            paramValue = self.enterText(msg="Please enter the chosen ID in the right format: ")
-            self.searchParams[param] = paramValue
+            pMsg = "How would you like to search for the paper?"
+            pOptions = ["S2PaperId", "CorpusId", "DOI", "ArXivId", "MAG", "ACL", "PMID", "PMCID", "URL", "Search by title"]
 
-        elif param == "Search by title":
-            paramValue = self.enterText(msg="Please enter the title of the paper: ")
-            self.searchParams[param] = paramValue
+            param = self.chooseOption(msg=pMsg, options=pOptions)
+
+            if param in pOptions and not (param == "Search by title"):
+                paramValue = self.enterText(msg="Please enter the chosen ID in the right format: ")
+                paperList.append(paramValue)
+                self.searchParams["paper_ids"] = paperList
+
+            elif param == "Search by title":
+                paramValue = self.enterText(msg="Please enter the title of the paper: ")
+                paperList.append(paramValue)
+                self.searchParams["query"] = paperList
+
+            #asking for specific fields not implemented: Import question!!
+
+            if self.chooseOption(msg="Would you like to search for another paper?", options=["YES", "NO"]) == "NO":
+                morePapers = False
         
 
     def authorUI(self) -> None:
-        '''Not implemented'''
-        print("Success!")
+
+        '''Ask user to enter search parameters for distinctive author search'''
+
+        authorList = list[str]
+        moreAuthors = True
+
+        while moreAuthors:
+
+            aMsg = "How would you like to search for the author?"
+            aOptions = ["S2AuthorId", "Search by name"]
+
+            param = self.chooseOption(msg=aMsg, options=aOptions)
+
+            if param == "S2AuthorId":
+                paramValue = self.enterText(msg="Please enter the author ID in the right format: ")
+                authorList.append(paramValue)
+                self.searchParams["author_ids"] = authorList
+
+            elif param == "Search by name":
+                paramValue = self.enterText(msg="Please enter the name of the author: ")
+                authorList.append(paramValue)
+                self.searchParams["query"] = authorList
+
+            if self.chooseOption(msg="Would you like to search for another author?", options=["YES", "NO"]) == "NO":
+                moreAuthors = False
+
+
+            #asking for specific fields not implemented: Import question!!
 
     def keywordUI(self) -> None:
-        '''Not implemented'''
-        print("Success!")
 
+        '''Ask user to enter Searchstring and limitations for Keyword search'''
+        
+        query = self.enterText(msg="Please enter the Query for your Keyword search: ")
+        self.searchParams["query"] = query
 
+        year = self.enterText(msg="Please enter the yearspan for your Keyword search. You can press Enter if you don't wish to specify a yearspan: ")
+        self.searchParams["year"] = year
+
+        #!!! LIST NEEDED FOR PUB TYPES!!!
+
+        publication_types = self.enterText(msg="Please enter the publication types you'd like to include in your Keyword search. "+ 
+                                           "You can press Enter if you don't wish to specify publication types: ")
+        self.searchParams["publication_types"] = publication_types####
+
+        #!!! LIST NEEDED FOR VENUES!!!
+
+        venue = self.enterText(msg="Please enter the venues for your Keyword search: ")
+        self.searchParams["query"] = query####
+
+        #!!! LIST NEEDED FOR STUDY FIELDS!!!
+
+        fields_of_study = self.enterText(msg="Please enter the fields of study for your Keyword search: ")
+        self.searchParams["fields_of_study"] = fields_of_study####
+
+        open_access = self.chooseOption(msg="Would you like to solely include open Access PDF files to your search results?", options=["YES", "NO"])
+        if open_access == "YES":
+            self.searchParams["open_access_pdf"] = True
+        else:
+            self.searchParams["open_access_pdf"] = False
+
+        limit = self.enterText(msg="How many search results should the query include? Please enter a number between 1 and 1000: ")
+        self.searchParams["limit"] = limit.format(int)
+
+        #asking for specific fields not implemented: Import question!!
 
 
     def chooseOption(self,
@@ -84,7 +153,6 @@ class Semanticscholar_ui():
     def enterText(self,
                   *,
                   msg: str,
-                  options
                   ) -> str:
         question = [inquirer.Text(name="Entry", message=msg,)]
         choice = inquirer.prompt(questions=question)
