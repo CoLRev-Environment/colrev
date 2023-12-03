95a2872c4dd31989dc92349a776fd8c0f607f56e
Peter Eckhardt <peter.eckhardt@stud.uni-bamberg.de>
2023-12-03 07:53:34 +0100

    Add evaluation for ui, func: __get_s2_parameters

--- colrev/ops/built_in/search_sources/semanticscholar.py
+++ colrev/ops/built_in/search_sources/semanticscholar.py
@@ -4,6 +4,7 @@
 from __future__ import annotations
 import json
 import typing
+import semanticscholarui
 
 from dataclasses import dataclass
 from multiprocessing import Lock
@@ -35,6 +36,7 @@ from semanticscholar import SemanticScholar
 if TYPE_CHECKING:
     import colrev.ops.search
     import colrev.ops.prep
+
 
 # pylint: disable=unused-argument
 # pylint: disable=duplicate-code
@@ -71,40 +73,48 @@ class SemanticScholarSearchSource(JsonSchemaMixin):
     __availability_exception_message = (
         f"Semantic Scholar ({Colors.ORANGE}check https://status.api.semanticscholar.org/{Colors.END})")
 
+    __s2_UI__ = semanticscholarui.SemanticScholarUI()
+    __s2__ = SemanticScholar
+
+    __search_return__ = ''
+
     def __init__(
             self,
             *,
             source_operation: colrev.operation.Operation,
             settings: typing.Optional[dict] = None,
     ) -> None:
+
         self.review_manager = source_operation.review_manager
-#        if settings:
-#            # Semantic Scholar as a search source
-#            self.search_source = from_dict(
-#                data_class=self.settings_class, data=settings
-#            )
-#        else:
-#            # Semantic Scholar as a md-prep source
-#            s2_api_source_l = [
-#                s
-#                for s in self.review_manager.settings.sources
-#                if s.filename == self.__s2_api_filename
-#            ]
-#            if s2_api_source_l:
-#                self.search_source = s2_api_source_l[0]
-#            else:
-#                self.search_source = colrev.settings.SearchSource(
-#                    endpoint="colrev.semanticscholar",
-#                    filename=self.__S2_md_filename,
-#                    search_type=colrev.settings.SearchType.API,
-#                    search_parameters={},
-#                    comment="",
-#                )
-#            self.s2_lock = Lock()
+        #        if settings:
+        #            # Semantic Scholar as a search source
+        #            self.search_source = from_dict(
+        #                data_class=self.settings_class, data=settings
+        #            )
+        #        else:
+        #            # Semantic Scholar as a md-prep source
+        #            s2_api_source_l = [
+        #                s
+        #                for s in self.review_manager.settings.sources
+        #                if s.filename == self.__s2_api_filename
+        #            ]
+        #            if s2_api_source_l:
+        #                self.search_source = s2_api_source_l[0]
+        #            else:
+        #                self.search_source = colrev.settings.SearchSource(
+        #                    endpoint="colrev.semanticscholar",
+        #                    filename=self.__S2_md_filename,
+        #                    search_type=colrev.settings.SearchType.API,
+        #                    search_parameters={},
+        #                    comment="",
+        #                )
+        #            self.s2_lock = Lock()
 
         self.language_service = colrev.env.language_service.LanguageService()
         # self.etiquette = self.get_etiquette(review_manager=self.review_manager)
         # self.email = self.review_manager.get_committer()
+
+        __search_return__ = ''
 
     def __query(self, **kwargs) -> typing.Iterator[dict]:  # type: ignore
         """Get records from Semantic Scholar based on a bibliographic query"""
@@ -112,31 +122,113 @@ class SemanticScholarSearchSource(JsonSchemaMixin):
         # Here don't use works or swagger_ui --> this is only for Crossref
         # We have to use semanticscholar (py) for that
 
-        #works = Works(etiquette=self.etiquette)
+        # works = Works(etiquette=self.etiquette)
         # use facets:
         # https://api.crossref.org/swagger-ui/index.html#/Works/get_works
 
-        #crossref_query_return = works.query(**kwargs).sort("deposited").order("desc")
-        #yield from crossref_query_return
+        # crossref_query_return = works.query(**kwargs).sort("deposited").order("desc")
+        # yield from crossref_query_return
 
-    def __get_s2_query_return(self, *, rerun: bool) -> typing.Iterator[dict]:
-        # Here you potentially get the query parameters
-        params = self.search_source.search_parameters
+    def __get_s2_parameters(self,
+                            *,
+                            rerun: bool
+                            ) -> typing.Iterator[dict]:
+        """Get all parameters from the user, using the User Interface"""
+        self.__s2_UI__.mainUI()
+        search_subject = self.__s2_UI__.searchSubject
+        params = self.__s2_UI__.searchParams
 
-        if not ("query" in params):
-            # Raising an exception
-            # but maybe check that earlier, because without query there is no json with the url
-            raise
+        # Open Semantic Scholar depending from the search subject  and look for search parameters
+        match search_subject:
+            case "keyword":
+                __search_return__ = self.keyword_search(params=params, rerun=rerun)
+            case "paper":
+                __search_return__ = self.paper_search(params=params, rerun=rerun)
+            case "author":
+                __search_return__ = self.author_search(params=params, rerun=rerun)
+            case _:
+                self.review_manager.logger.info(
+                    "No search parameters were found."
+                )
 
-        if ("\"total\": 0" in params):
-            self.review_manager.logger.info("Nothing was found with the given search parameters: " + params)
-            raise
+    #        if ("\"total\": 0" in params):
+    #            self.review_manager.logger.info("Nothing was found with the given search parameters: " + params)
+    #            raise
 
-        s2_query = {"bibliographic": params["query"].replace(" ", "+")}
+    def keyword_search(self,
+                       *,
+                       params: dict,
+                       rerun: bool
+                       ) -> dict:
 
+        query = ''
+        year = ''
+        publication_types = ''
+        venue = ''
+        fields_of_study = ''
+        open_access_pdf = ''
+        limit = ''
 
+        for key, value in params.items():
+            match key:
+                case "query":
+                    query = value
+                case "year":
+                    year = value
+                case "publication_types":
+                    publication_types = value
+                case "venue":
+                    venue = value
+                case "fields_of_study":
+                    fields_of_study = value
+                case "open_access_pdf":
+                    open_access_pdf = value
+                case "limit":
+                    limit = value
 
+        record_return = self.__s2__.search_paper(query=query,
+                                                 year=year,
+                                                 publication_types=publication_types,
+                                                 venue=venue,
+                                                 fields_of_study=fields_of_study,
+                                                 open_access_pdf=open_access_pdf,
+                                                 limit=limit,
+                                                 )
+        return record_return
 
+    def paper_search(self,
+                     *,
+                     params: dict,
+                     rerun: bool
+                     ) -> dict:
+        for key, value in params.items():
+            if key == "paper_ids":
+                record_return = self.__s2__.get_papers(value)
+            elif key == "query":
+                record_return = self.__s2__.search_paper(value)
+            else:
+                self.review_manager.logger.info(
+                    "Search type \"Search for paper\" is not available with your parameters.\n"
+                    + "Search parameter: "
+                    + value
+                )
+
+    def author_search(self,
+                      *,
+                      params: dict,
+                      rerun: bool
+                      ) -> dict:
+        for key, value in params.items():
+            if key == "author_ids":
+                record_return = self.__s2__.get_authors(value)
+            elif key == "queryList":
+                record_return = self.__s2__.search_author(value)
+            else:
+                self.review_manager.logger.info(
+                    "Search type \"Search for author\" is not available with your parameters.\n"
+                    + "Search parameter: "
+                    + value
+                )
 
     def run_search(
             self,
@@ -144,20 +236,28 @@ class SemanticScholarSearchSource(JsonSchemaMixin):
             rerun: bool
     ) -> None:
         """Run a search of Semantic Scholar"""
-        # validate
-        try:
-            if rerun:
-                self.review_manager.logger.info(
-                    "Performing a search of the full history (may take time)"
-                )
 
+        # validate source
+
+        s2_feed = self.search_source.get_feed(
+            review_manager=self.review_manager,
+            source_identifier=self.source_identifier,
+            update_only=(not rerun)
+        )
+
+        if rerun:
+            self.review_manager.logger.info(
+                "Performing a search of the full history (may take time)"
+            )
+
+        try:
             # no full rerun
             # load the database (feed) --> __get_masterdata_record() and __get_masterdata()
             # or records = self.review_manager.dataset.load_records_dict()
             records = self.review_manager.dataset.load_records_dict()
 
             try:
-                for item in self.__get_s2_query_return(rerun=rerun):
+                for item in self.__get_s2_parameters(rerun=rerun):
                     try:
                         retrieved_record_dict = connector_utils.json_to_record(item=item)
                         continue
@@ -165,7 +265,6 @@ class SemanticScholarSearchSource(JsonSchemaMixin):
                         raise
             except:
                 raise
-
 
             self.review_manager.dataset.save_records_dict(records=records)
         # pip install requests
