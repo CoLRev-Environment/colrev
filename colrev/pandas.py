17ed229df63d483982f0eeb9a2960f7ce41c53f7
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2024-02-08 11:12:36 +0100

    add pandas: load_resolved_papers()

--- colrev/pandas.py
+++ colrev/pandas.py
@@ -9,6 +9,9 @@ from typing import List
 from typing import Optional
 
 import pandas as pd
+from bib_dedupe.bib_dedupe import block
+from bib_dedupe.bib_dedupe import match
+from bib_dedupe.bib_dedupe import prep
 
 import colrev.review_manager
 from colrev.constants import Fields
@@ -42,6 +45,106 @@ def load_df(
     records = review_manager.dataset.load_records_dict()
     df = pd.DataFrame.from_dict(records, orient="index")
     return df
+
+
+def load_resolved_papers(other_project_path: str) -> pd.DataFrame:
+    """Loads records from the other project, matches them with the current project,
+    and returns a DataFrame of the other records with the corresponding IDs in the current project.
+
+    This is useful when integrating datasets and reusing screening decisions or coding.
+
+    Example:
+
+    Current project record(s):
+
+    @Webster2002{
+        title = "Analyzing the past to prepare for the future",
+        author = "Webser, Jane and Watson, Rick",
+        ...
+    }
+
+    Other project record(s):
+
+    @ID1{
+        title = "ANALYZING THE PAST TO PREPARE FOR THE FUTURE",
+        author = "WEBSTER; WATSON",
+        colrev_status = "rev_included",
+        methods = "literature_review",
+        ...
+    }
+
+    Returns:
+
+    @Webster2002{
+        title = "ANALYZING THE PAST TO PREPARE FOR THE FUTURE",
+        author = "WEBSTER; WATSON",
+        colrev_status = "rev_included",
+        methods = "literature_review",
+        OTHER_ID = "ID1",
+        ...
+    }
+
+    Data can easily be assigned to the current project
+    by accessing other records based on corresponding current IDs.
+
+    """
+
+    other_review_manager = colrev.review_manager.ReviewManager(
+        path_str=other_project_path, force_mode=True
+    )
+    colrev.operation.CheckOperation(review_manager=other_review_manager)
+    other_records = other_review_manager.dataset.load_records_dict()
+    project_path = str(Path.cwd())
+    review_manager = colrev.review_manager.ReviewManager(
+        path_str=project_path, force_mode=True
+    )
+    colrev.operation.CheckOperation(review_manager=review_manager)
+    records = review_manager.dataset.load_records_dict()
+
+    # identify duplicates with dedupe
+
+    records_df = pd.DataFrame.from_dict(records, orient="index")
+    records_df["search_set"] = "ORIGINAL"
+    records_df[Fields.ID] = "ORIGINAL_" + records_df[Fields.ID].astype(str)
+    other_records_df = pd.DataFrame.from_dict(other_records, orient="index")
+    other_records_df["search_set"] = "OTHER"
+    other_records_df.reset_index(inplace=True)
+
+    combined_df = pd.concat([records_df, other_records_df], ignore_index=True)
+
+    selected_columns = [
+        Fields.ID,
+        "search_set",
+        Fields.AUTHOR,
+        Fields.TITLE,
+        Fields.YEAR,
+        Fields.JOURNAL,
+        Fields.BOOKTITLE,
+        Fields.VOLUME,
+        Fields.NUMBER,
+        Fields.PAGES,
+        Fields.DOI,
+        Fields.ENTRYTYPE,
+        Fields.ABSTRACT,
+    ]
+    combined_df = combined_df[selected_columns]
+
+    combined_df = prep(records_df=combined_df)
+
+    deduplication_pairs = block(combined_df)
+    matched_df = match(deduplication_pairs)
+
+    matched_df.rename(columns={"ID_2": "ID"}, inplace=True)
+    matched_df = matched_df[matched_df["duplicate_label"] == "duplicate"]
+
+    merged_df = pd.merge(other_records_df, matched_df, on="ID", how="inner")
+
+    merged_df["other_ID"] = merged_df["ID"]
+    merged_df[Fields.ID] = merged_df["ID_1"].str.replace("ORIGINAL_", "")
+    merged_df.drop(columns=["ID_1"], inplace=True)
+    merged_df.drop(columns=["search_set", "duplicate_label", "index"], inplace=True)
+
+    return merged_df
 
 
 def add_from_tei(
