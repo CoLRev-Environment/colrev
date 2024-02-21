4106aed34f8d64c1c355b55ec7c2d3491c6086af
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2024-02-21 15:09:50 +0100

    extend tests for dataset

--- tests/0_core/dataset_test.py
+++ tests/0_core/dataset_test.py
@@ -1,10 +1,90 @@
 #!/usr/bin/env python
 """Tests for the dataset"""
+import git
 import pytest
 
 import colrev.review_manager
 
 # flake8: noqa: E501
+
+
+def test_get_origin_state_dict(
+    base_repo_review_manager: colrev.review_manager.ReviewManager,
+) -> None:
+    """Test the retrieval of origin state dictionary."""
+
+    origin_state_dict = base_repo_review_manager.dataset.get_origin_state_dict()
+
+    expected_dict = {
+        "test_records.bib/Srivastava2015": colrev.record.RecordState.pdf_needs_manual_retrieval
+    }
+
+    assert (
+        origin_state_dict == expected_dict
+    ), "The origin state dictionary does not match the expected output."
+
+
+def test_get_committed_origin_state_dict(
+    base_repo_review_manager: colrev.review_manager.ReviewManager,
+) -> None:
+    """Test the retrieval of committed origin state dictionary."""
+
+    committed_origin_state_dict = (
+        base_repo_review_manager.dataset.get_committed_origin_state_dict()
+    )
+
+    expected_dict = {
+        "test_records.bib/Srivastava2015": colrev.record.RecordState.pdf_needs_manual_retrieval
+    }
+
+    assert (
+        committed_origin_state_dict == expected_dict
+    ), "The committed origin state dictionary does not match the expected output."
+
+
+def test_get_changed_records(
+    base_repo_review_manager: colrev.review_manager.ReviewManager,
+) -> None:
+    """Test the retrieval of changed records based on a selected commit."""
+
+    # Simulate changes in records and commit those changes
+    changed_record_dict = {
+        "Srivastava2015": {
+            "colrev_origin": "test_records.bib/Srivastava2015",
+            "colrev_status": colrev.record.RecordState.pdf_prepared,
+            "ID": "Srivastava2015",
+            "ENTRYTYPE": "article",
+            "author": "Srivastava, Shirish C. and Shainesh, G.",
+            "journal": "Nature",
+            "title": "Bridging the service divide through digitally enabled service innovations: Evidence from Indian healthcare service providers",
+            "year": "2015",
+            "volume": "39",
+            "number": "1",
+            "pages": "245--267",
+            "language": "eng",
+        }
+    }
+    base_repo_review_manager.dataset.save_records_dict(records=changed_record_dict)
+    base_repo_review_manager.dataset._add_record_changes()
+    commit_message = "Test commit for changed records"
+    author = git.Actor("Author Name", "author@example.com")
+    committer = git.Actor("Committer Name", "committer@example.com")
+    base_repo_review_manager.dataset.create_commit(
+        msg=commit_message, author=author, committer=committer, hook_skipping=True
+    )
+
+    # Retrieve the last commit SHA
+    last_commit_sha = base_repo_review_manager.dataset.get_last_commit_sha()
+
+    # Retrieve changed records based on the last commit
+    changed_records = base_repo_review_manager.dataset.get_changed_records(
+        target_commit=last_commit_sha
+    )
+    expected_changes = [{'ID': 'Srivastava2015', 'ENTRYTYPE': 'article', 'colrev_origin': ['test_records.bib/Srivastava2015'], 'colrev_status': colrev.record.RecordState.pdf_prepared, 'journal': 'Nature', 'title': 'Bridging the service divide through digitally enabled service innovations: Evidence from Indian healthcare service providers', 'year': '2015', 'volume': '39', 'number': '1', 'pages': '245--267', 'language': 'eng', 'author': 'Srivastava, Shirish C. and Shainesh, G.', 'changed_in_target_commit': 'True'}]
+    # Check if the changed records match the expected changes
+    assert (
+        changed_records == expected_changes
+    ), "The retrieved changed records do not match the expected changes."
 
 
 @pytest.mark.parametrize(
