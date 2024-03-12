d675e9ae8d06eff933fb91d9f1d646d352825600
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2024-03-12 09:03:28 +0100

    add ris/fix

--- colrev/loader/ris.py
+++ colrev/loader/ris.py
@@ -121,3 +121,28 @@ class RISLoader(colrev.loader.loader.Loader):
         records_list = list(r for r in self._parse_lines(lines) if r)
 
         return records_list
+
+    def apply_ris_fixes(self) -> None:
+        """Fix common defects in RIS files"""
+
+        # Error to fix: for lists of keywords, each line should start with the KW tag
+
+        with open(self.filename, encoding="UTF-8") as file:
+            lines = [line.rstrip("\n") for line in file]
+            # add missing start tags in lists (like KW)
+            processing_tag = ""
+            for i, line in enumerate(lines):
+                tag_match = re.match(r"^[A-Z][A-Z0-9]+(\s+)-", line)  # |^ER\s?|^EF\s?
+                if tag_match:
+                    processing_tag = tag_match.group()
+                elif line == "":
+                    processing_tag = ""
+                    continue
+                elif processing_tag == "":
+                    continue
+                else:
+                    lines[i] = f"{processing_tag} {line}"
+
+        with open(self.filename, "w", encoding="utf-8") as file:
+            for line in lines:
+                file.write(f"{line}\n")
