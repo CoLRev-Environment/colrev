ef90dd71a4993eff67e4eae4607d042f49494115
JohannesDiel <johannes-m.diel@gmx.de>
2024-05-31 16:57:39 +0200

    Load function for saved data

--- colrev/packages/osf_searchsource/src/osf_searchsource.py
+++ colrev/packages/osf_searchsource/src/osf_searchsource.py
@@ -10,6 +10,8 @@ import zope.interface
 from dacite import from_dict
 from dataclasses_jsonschema import JsonSchemaMixin
 
+import colrev.loader.load_utils
+import colrev.ops.load
 import colrev.ops.prep
 import colrev.package_manager.interfaces
 import colrev.package_manager.package_manager
@@ -30,9 +32,11 @@ from colrev.constants import SearchType
 @dataclass
 class OSFSearchSource(JsonSchemaMixin):
     """OSF"""
-    def __init__(self,*):
-    
-
+    def __init__(self,* ,source_operation: colrev.process.operation.Operation, settings: dict
+    ) -> None:
+       
+        self.search_source = from_dict(data_class=self.settings_class, data=settings)
+        self.review_manager = source_operation.review_manager
 
     def heuristic():
 
@@ -55,14 +59,29 @@ class OSFSearchSource(JsonSchemaMixin):
 
 
     def _create_record_dict():
+       #need info about the data retrieved from the api to turn the file(s) into a .bib file for further analysis
 
+    def _load_bib(self) -> dict:
 
-    def load():
+        records = colrev.loader.load_utils.load(
+            filename=self.search_source.filename,
+            logger=self.review_manager.logger,
+            unique_id_field="ID",
+        )
+        for record_dict in records.values():
+            record_dict.pop("type", None)
 
+        return records
+
+    def load(self, load_operation: colrev.ops.load.Load) -> dict:
+        """Load the records from the SearchSource file"""
+          if self.search_source.filename.suffix == ".bib":
+            return self._load_bib()
+        raise NotImplementedError
 
 
     def prepare():
-
+        #need the dict method to create the prepare method
 
 
     
