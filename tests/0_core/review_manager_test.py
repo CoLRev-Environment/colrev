d375ba17d91cb3313b40a61ad387ea81de872779
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2024-06-15 11:01:38 +0200

    release 0.12.3

--- tests/0_core/review_manager_test.py
+++ tests/0_core/review_manager_test.py
@@ -52,7 +52,7 @@ def test_get_colrev_versions(
     base_repo_review_manager: colrev.review_manager.ReviewManager,
 ) -> None:
     colrev_versions = base_repo_review_manager.get_colrev_versions()
-    assert ["0.12.2", "0.12.2"] == colrev_versions
+    assert ["0.12.3", "0.12.3"] == colrev_versions
 
 
 def test_check_repository_setup(
