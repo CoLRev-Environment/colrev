ca421f83eaf4614f35b4b773f546b1bad995541f
koljarinne <167416691+koljarinne@users.noreply.github.com>
2024-06-19 13:47:53 +0000

    added exeption handling for repo_to_records

--- colrev/packages/github/src/github_search_source.py
+++ colrev/packages/github/src/github_search_source.py
@@ -31,7 +31,7 @@ from github import Github
 # Authentication is defined via github.Auth
 from github import Auth
 # using an access token
-auth = Auth.Token("access_token")
+auth = Auth.Token("token here")
 
 # First create a Github instance:
 # Public Web Github
@@ -242,25 +242,36 @@ class GitHubSearchSource(JsonSchemaMixin):
 
             results = []
             for repo in repositories:
-                repo_data = {
-                    "name": repo.name,
-                    "full_name": repo.full_name,
-                    "description": repo.description,
-                    "html_url": repo.html_url,
-                    "created_at": repo.created_at.isoformat(),
-                    "updated_at": repo.updated_at.isoformat(),
-                    "pushed_at": repo.pushed_at.isoformat(),
-                    "stargazers_count": repo.stargazers_count,
-                    "language": repo.language,
-                }
-                
-                #results.append(connector_utils.repo_to_record(repo=repo))
-                results.append(repo_data)
+               #repo_data = {
+               #    "name": repo.name,
+               #    "full_name": repo.full_name,
+               #    "description": repo.description,
+               #    "html_url": repo.html_url,
+               #    "created_at": repo.created_at.isoformat(),
+               #    "updated_at": repo.updated_at.isoformat(),
+               #    "pushed_at": repo.pushed_at.isoformat(),
+               #    "stargazers_count": repo.stargazers_count,
+               #    "language": repo.language,
+               #}
+                try:
+                    repo=connector_utils.repo_to_record(repo=repo)
+                    print(repo)
+                    results.append(repo)
+                    pass
+                except Exception as e:
+                    print("Skipped because there was an Error: ")
+                    pass
+                #results.append(repo_data)
                 
 
             # Speichern der Ergebnisse in einer JSON-Datei
-            with open(self._github_md_filename, 'w', encoding='utf-8') as f:
-                json.dump(results, f, ensure_ascii=False, indent=4)
+            #with open(self._github_md_filename, 'w', encoding='utf-8') as f:
+            #    json.dump(results, f, ensure_ascii=False, indent=4)
+
+            # Speichern als Repos in .bib
+            with open(self._github_md_filename, 'w') as file:
+                for repo in results:
+                    file.write(repo)        #Speichern so nicht möglich, da repo kein string
 
             # Schließen der GitHub-Verbindung
             g.close()
