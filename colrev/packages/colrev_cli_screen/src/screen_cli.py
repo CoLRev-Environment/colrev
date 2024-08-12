cfc05734583e3c8c3ff7c6a5a56708fbec66e785
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2024-08-12 08:59:49 +0200

    screen-cli: add validation

--- colrev/packages/colrev_cli_screen/src/screen_cli.py
+++ colrev/packages/colrev_cli_screen/src/screen_cli.py
@@ -94,7 +94,8 @@ class CoLRevCLIScreen(JsonSchemaMixin):
                 == criterion_settings.criterion_type
             ):
                 color = Colors.RED
-            choices.append(f"{color}{criterion_name}{Colors.END}")
+            choices.append(f"{color}{criterion_name}{Colors.END} violated")
+        choices.append("Include (no criterion violated)")
         choices.append("Skip")
         choices.append("Quit")
         question = [
@@ -106,8 +107,40 @@ class CoLRevCLIScreen(JsonSchemaMixin):
             ),
         ]
 
-        answers = prompt(question)
-        violated_criteria = answers["violated_criteria"]
+        while True:
+            answers = prompt(question)
+            violated_criteria = answers["violated_criteria"]
+
+            # can only select one of the options quit, skip, or include/no criterion violated
+            if "Quit" in violated_criteria and len(violated_criteria) > 1:
+                print(
+                    "Contradiction: 'Quit' and other criteria selected. Please select again."
+                )
+                continue
+            if "Skip" in violated_criteria and len(violated_criteria) > 1:
+                print(
+                    "Contradiction: 'Skip' and other criteria selected. Please select again."
+                )
+                continue
+            if (
+                "Include (no criterion violated)" in violated_criteria
+                and len(violated_criteria) > 1
+            ):
+                print(
+                    "Contradiction: 'Include (no criterion violated)'"
+                    " and other criteria selected. Please select again."
+                )
+                continue
+            # prevent contradiction (include and criterion violated)
+            if "Include (no criterion violated)" in violated_criteria:
+                violated_criteria.remove("Include (no criterion violated)")
+                if violated_criteria:
+                    print(
+                        "Contradiction: 'Include (no criterion violated)' "
+                        "and other criteria selected. Please select again."
+                    )
+                    continue
+            break
 
         if "Skip" in violated_criteria:
             return "skip"
@@ -115,12 +148,14 @@ class CoLRevCLIScreen(JsonSchemaMixin):
             return "quit"
 
         decisions = {c: "in" for c in self.screening_criteria.keys()}
-        for criterion_name in violated_criteria:
-            decisions[
-                criterion_name.replace(Colors.RED, "")
-                .replace(Colors.END, "")
-                .replace(Colors.GREEN, "")
-            ] = "out"
+        if "Include (no criterion violated)" not in violated_criteria:
+            for criterion_name in violated_criteria:
+                decisions[
+                    criterion_name.replace(Colors.RED, "")
+                    .replace(Colors.END, "")
+                    .replace(Colors.GREEN, "")
+                    .replace(" violated", "")
+                ] = "out"
 
         c_field = ""
         for criterion_name, decision in decisions.items():
