c4c6e0eda731ae117cdf23ccaedd2b7507de3787
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2024-09-03 21:13:13 +0200

    format and docs

--- colrev/ui_cli/install.py
+++ colrev/ui_cli/install.py
@@ -63,32 +63,20 @@ def _get_colrev_path() -> Path:
 
     local_editable_colrev_path = _get_local_editable_colrev_path()
     if local_editable_colrev_path:
-        print(f"Local editable colrev path: {local_editable_colrev_path}")
         colrev_path = Path(local_editable_colrev_path)
-        # Check if the path starts with a slash and contains a colon (e.g., /D:/)
-        print(colrev_path)
-        print(local_editable_colrev_path)
         if local_editable_colrev_path.startswith("/D:"):
-            print('HERE')
             # Remove the leading slash to correct the path format
             corrected_path_str = local_editable_colrev_path.lstrip("/")
             colrev_path = Path(corrected_path_str).resolve(strict=False)
         else:
             colrev_path = colrev_path.resolve(strict=False)
-        print(f"Resolved local colrev path: {colrev_path}")
     else:
         colrev_path = _clone_colrev_repository().resolve(strict=False)
-        print(f"Resolved cloned colrev path: {colrev_path}")
 
     # Check if there is a nested colrev directory
     potential_nested_path = colrev_path / "colrev"
     if potential_nested_path.is_dir():
         colrev_path = potential_nested_path
-        print(f"Using nested colrev path: {colrev_path}")
-    
-    # list files in colrev_path
-    for item in colrev_path.iterdir():
-        print(f"Item: {item}")
 
     return colrev_path
 
