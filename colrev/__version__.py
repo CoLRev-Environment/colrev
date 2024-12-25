333a374ebd1259c2d0647a79aead5b619b121ee5
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2024-12-25 10:43:59 +0100

    version: drop pre-python3.8

--- colrev/__version__.py
+++ colrev/__version__.py
@@ -1,8 +1,4 @@
 # pylint: disable=missing-module-docstring
-try:
-    from importlib.metadata import version
-except ImportError:  # pragma: no cover
-    # For Python < 3.8
-    from importlib_metadata import version  # type: ignore
+from importlib.metadata import version
 
 __version__ = version("colrev")
