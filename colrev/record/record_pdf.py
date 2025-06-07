16f8f3564507cac3613c04eeb9ebfb33b26c8d21
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2025-06-07 14:44:24 +0200

    PDFRecord: require path as a keyword argument

--- colrev/record/record_pdf.py
+++ colrev/record/record_pdf.py
@@ -23,7 +23,7 @@ from colrev.constants import Fields
 class PDFRecord(colrev.record.record.Record):
     """The PDFRecord class provides a range of Function for PDF handling"""
 
-    def __init__(self, data: dict, path: Path) -> None:
+    def __init__(self, data: dict, *, path: Path) -> None:
         self.data = data
         """Dictionary containing the record data"""
 
