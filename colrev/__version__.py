3aac829736b8bbb8fc664efd26fb985db813fea4
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2025-08-03 11:29:43 +0200

    update

--- colrev/__version__.py
+++ colrev/__version__.py
@@ -1,7 +1,4 @@
 # pylint: disable=missing-module-docstring
-from importlib.metadata import PackageNotFoundError, version
+from importlib.metadata import version
 
-try:
-    __version__ = version("colrev")
-except PackageNotFoundError:
-    __version__ = "0.0.0"
+__version__ = version("colrev")
