ecc2285de36cd6f12a870602da28da2c746d6508
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2025-08-06 10:42:15 +0200

    update package-init searchsource

--- tests/data/package_init/search_source.py
+++ tests/data/package_init/search_source.py
@@ -11,7 +11,7 @@ from colrev.package_manager.package_base_classes import SearchSourcePackageBaseC
 
 class CustomName(SearchSourcePackageBaseClass):
 
-    def __init__(self, *, source_operation: 'colrev.process.operation.Operation', settings: 'dict', logger: 'Optional[logging.Logger]' = None, verbose_mode: 'bool' = False) -> 'None':
+    def __init__(self, *, source_operation: 'colrev.process.operation.Operation', settings: 'colrev.search_file.ExtendedSearchFile', logger: 'Optional[logging.Logger]' = None, verbose_mode: 'bool' = False) -> 'None':
         """Initialize self.  See help(type(self)) for accurate signature."""
 
     def prep_link_md(self, prep_operation: 'colrev.ops.prep.Prep', record: 'colrev.record.record.Record', save_feed: 'bool' = True, timeout: 'int' = 10) -> 'colrev.record.record.Record':
