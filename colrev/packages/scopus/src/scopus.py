a1a92bde74067966bbfc813deee585c965599243
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2025-09-17 08:16:06 +0200

    update scopus

--- colrev/packages/scopus/src/scopus.py
+++ colrev/packages/scopus/src/scopus.py
@@ -3,8 +3,8 @@
 from __future__ import annotations
 
 import logging
+import typing
 from pathlib import Path
-from typing import Optional
 
 from pydantic import Field
 
@@ -42,7 +42,7 @@ class ScopusSearchSource(base_classes.SearchSourcePackageBaseClass):
         self,
         *,
         search_file: colrev.search_file.ExtendedSearchFile,
-        logger: Optional[logging.Logger] = None,
+        logger: typing.Optional[logging.Logger] = None,
         verbose_mode: bool = False,
     ) -> None:
         self.logger = logger or logging.getLogger(__name__)
@@ -93,7 +93,7 @@ class ScopusSearchSource(base_classes.SearchSourcePackageBaseClass):
         cls,
         params: str,
         path: Path,
-        logger: Optional[logging.Logger] = None,
+        logger: typing.Optional[logging.Logger] = None,
     ) -> colrev.search_file.ExtendedSearchFile:
         """Add SearchSource as an endpoint (based on query provided to colrev search --add )"""
 
