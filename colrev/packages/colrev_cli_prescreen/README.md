57a5d101d50c4469f4221d080d907d120badb987
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2026-01-28 14:08:05 +0100

    prescreen-cli

--- colrev/packages/colrev_cli_prescreen/README.md
+++ colrev/packages/colrev_cli_prescreen/README.md
@@ -3,3 +3,12 @@
 ## prescreen
 
 Interactively prescreen records using the CLI.
+
+
+## Prescreening BibTex
+
+The following cli tool supports pre-screening outside of CoLRev projects:
+
+```sh
+colrev-cli-prescreen
+```
--- colrev/packages/colrev_cli_prescreen/src/prescreen_cli.py
+++ colrev/packages/colrev_cli_prescreen/src/prescreen_cli.py
@@ -14,9 +14,42 @@ import colrev.record.record
 from colrev.constants import Colors
 from colrev.constants import ENTRYTYPES
 from colrev.constants import Fields
+from colrev.constants import RecordState
 
 
 # pylint: disable=too-few-public-methods
+
+
+def _print_prescreen_record(record_dict: dict) -> None:
+    """Print the record for prescreen operations"""
+
+    ret_str = f"  ID: {record_dict['ID']} ({record_dict[Fields.ENTRYTYPE]})"
+    ret_str += (
+        f"\n  {Colors.GREEN}{record_dict.get(Fields.TITLE, 'no title')}{Colors.END}"
+        f"\n  {record_dict.get(Fields.AUTHOR, 'no-author')}"
+    )
+    if record_dict[Fields.ENTRYTYPE] == ENTRYTYPES.ARTICLE:
+        ret_str += (
+            f"\n  {record_dict.get(Fields.JOURNAL, 'no-journal')} "
+            f"({record_dict.get(Fields.YEAR, 'no-year')}) "
+            f"{record_dict.get(Fields.VOLUME, 'no-volume')}"
+            f"({record_dict.get(Fields.NUMBER, '')})"
+        )
+    elif record_dict[Fields.ENTRYTYPE] == ENTRYTYPES.INPROCEEDINGS:
+        ret_str += f"\n  {record_dict.get(Fields.BOOKTITLE, 'no-booktitle')}"
+    if Fields.ABSTRACT in record_dict:
+        lines = textwrap.wrap(record_dict[Fields.ABSTRACT], 100, break_long_words=False)
+        if lines:
+            ret_str += f"\n  Abstract: {lines.pop(0)}\n"
+            ret_str += "\n  ".join(lines) + ""
+
+    if Fields.URL in record_dict:
+        ret_str += f"\n  url: {record_dict[Fields.URL]}"
+
+    if Fields.FILE in record_dict:
+        ret_str += f"\n  file: {record_dict[Fields.FILE]}"
+
+    print(ret_str)
 
 
 class CoLRevCLIPrescreen(base_classes.PrescreenPackageBaseClass):
@@ -37,39 +70,6 @@ class CoLRevCLIPrescreen(base_classes.PrescreenPackageBaseClass):
         self.settings = self.settings_class(**settings)
         self.prescreen_operation = prescreen_operation
         self.review_manager = prescreen_operation.review_manager
-
-    def _print_prescreen_record(self, record: colrev.record.record.Record) -> None:
-        """Print the record for prescreen operations"""
-
-        ret_str = f"  ID: {record.data['ID']} ({record.data[Fields.ENTRYTYPE]})"
-        ret_str += (
-            f"\n  {Colors.GREEN}{record.data.get(Fields.TITLE, 'no title')}{Colors.END}"
-            f"\n  {record.data.get(Fields.AUTHOR, 'no-author')}"
-        )
-        if record.data[Fields.ENTRYTYPE] == ENTRYTYPES.ARTICLE:
-            ret_str += (
-                f"\n  {record.data.get(Fields.JOURNAL, 'no-journal')} "
-                f"({record.data.get(Fields.YEAR, 'no-year')}) "
-                f"{record.data.get(Fields.VOLUME, 'no-volume')}"
-                f"({record.data.get(Fields.NUMBER, '')})"
-            )
-        elif record.data[Fields.ENTRYTYPE] == ENTRYTYPES.INPROCEEDINGS:
-            ret_str += f"\n  {record.data.get(Fields.BOOKTITLE, 'no-booktitle')}"
-        if Fields.ABSTRACT in record.data:
-            lines = textwrap.wrap(
-                record.data[Fields.ABSTRACT], 100, break_long_words=False
-            )
-            if lines:
-                ret_str += f"\n  Abstract: {lines.pop(0)}\n"
-                ret_str += "\n  ".join(lines) + ""
-
-        if Fields.URL in record.data:
-            ret_str += f"\n  url: {record.data[Fields.URL]}"
-
-        if Fields.FILE in record.data:
-            ret_str += f"\n  file: {record.data[Fields.FILE]}"
-
-        print(ret_str)
 
     def _fun_cli_prescreen(
         self,
@@ -96,7 +96,7 @@ class CoLRevCLIPrescreen(base_classes.PrescreenPackageBaseClass):
 
             print("\n\n")
             print(f"Record {i} (of {stat_len})\n")
-            self._print_prescreen_record(record)
+            _print_prescreen_record(record.data)
 
             while ret not in ["y", "n", "s", "q"]:
                 ret = input(
@@ -118,6 +118,7 @@ class CoLRevCLIPrescreen(base_classes.PrescreenPackageBaseClass):
                 continue
 
             inclusion_decision = "yes" == inclusion_decision_str
+
             self.prescreen_operation.prescreen(
                 record=record,
                 prescreen_inclusion=inclusion_decision,
@@ -160,3 +161,48 @@ class CoLRevCLIPrescreen(base_classes.PrescreenPackageBaseClass):
             msg="Pre-screen: manual screen (cli)", manual_author=True
         )
         return records
+
+
+def cli() -> None:
+    import colrev.loader.load_utils
+    import colrev.writer.write_utils
+
+    # TODO: allow custom file selection
+    filename = "records.bib"
+    records_dict = colrev.loader.load_utils.load(filename=filename)
+    for record_dict in records_dict.values():
+        if record_dict["colrev_status"] != RecordState.md_processed:
+            continue
+
+        print("\n\n")
+        _print_prescreen_record(record_dict)
+        ret, inclusion_decision_str = "NA", "NA"
+        quit_pressed = False
+
+        while ret not in ["y", "n", "s", "q"]:
+            ret = input(
+                "\nInclude this record "
+                "[enter y,n,s,q for yes, no, skip/decide later, quit-and-save]? "
+            )
+            if ret == "q":
+                quit_pressed = True
+            elif ret == "s":
+                continue
+            else:
+                inclusion_decision_str = ret.replace("y", "yes").replace("n", "no")
+
+        if quit_pressed:
+            print("Stop prescreen")
+            break
+
+        if ret == "s":
+            continue
+
+        inclusion = "yes" == inclusion_decision_str
+
+        if inclusion:
+            record_dict["colrev_status"] = RecordState.rev_synthesized
+        else:
+            record_dict["colrev_status"] = RecordState.rev_excluded
+
+    colrev.writer.write_utils.write_file(records_dict, filename=filename)
